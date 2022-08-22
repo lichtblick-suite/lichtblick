lichtblick-suite/lichtblick

commit 0b5b6803f2b993275f2dd08f0e7ac75f0c9391fe

Kill fluentui makeStyles (this time it's personal) (#3987)
* components/Autocomplete: Remove use of fluentui/makeStyles

* panel/Teleop: Remove use of fluentui/makeStyles

* panels/useLogStyles: Remove use of fluentui/makeStyles

* panels/ImageCanvas: Remove use of fluentui/makeStyles

* component/TimeBasedChartTooltipContent: Remove use of fluentui/makeStyles

* Fix breaks caused by change to logStyles

* component/Badge: Remove use of fluentui/makeStyles

* Fix more fallout from change to useLogStyles

* Change line height to be explicitly pixels

* Fix line issue on components/Badge

* Explicitly make hasError a boolean

* Dial back autocomplete shadow a bit

* Split the difference on the elevation

* More Shadow diffing

* Update CssBaseline styles 🙏📿

* Remove line-height from CssBaseline

* Tune divider color

* More divider tuning

* Fix linter error

* Lighter divider color on light theme

* Refactor GlobalCss to use mui theming

* Update mui typography to be simililar to fluent default fonts

* Update places where slashed zero appears to be important

* Update raw message value to match current formatting

packages/studio-base/src/components/CssBaseline.tsx
@@ -2,15 +2,15 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { makeStyles } from "@fluentui/react";
 import { PropsWithChildren } from "react";
+import { makeStyles } from "tss-react/mui";
 
 import "@foxglove/studio-base/styles/assets/inter.css";
 import "@foxglove/studio-base/styles/assets/plex-mono.css";
 
-import { colors, fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
+import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()(({ palette, typography }) => ({
   root: {
     "*,*:before,*:after": {
       boxSizing: "inherit",
@@ -31,14 +31,14 @@ const useStyles = makeStyles((theme) => ({
         background: "transparent",
       },
       "::-webkit-scrollbar-thumb": {
-        background: theme.palette.blackTranslucent40,
+        background: palette.action.focus,
         borderRadius: 2,
       },
     },
     "p:not([class^='Mui')": {
       margin: "1em 0",
 
-      ":last-child": {
+      "&:last-child": {
         marginBottom: 0,
       },
     },
@@ -54,10 +54,10 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     flex: "1 1 100%",
     overflow: "hidden",
-    background: theme.semanticColors.bodyBackground,
-    color: theme.semanticColors.bodyText,
+    background: palette.background.default,
+    color: palette.text.primary,
     font: "inherit",
-    ...theme.fonts.small,
+    fontSize: typography.body2.fontSize,
     fontFeatureSettings: fonts.SANS_SERIF_FEATURE_SETTINGS,
 
     // mosaic styling
@@ -69,8 +69,8 @@ const useStyles = makeStyles((theme) => ({
         right: 0,
 
         ".drop-target-container .drop-target": {
-          backgroundColor: theme.palette.neutralQuaternary,
-          border: `2px solid ${theme.palette.neutralDark}`,
+          backgroundColor: palette.action.hover,
+          border: `2px solid ${palette.divider}`,
         },
         ".drop-target-container .drop-target-hover": {
           opacity: 0.3,
@@ -139,20 +139,19 @@ const useStyles = makeStyles((theme) => ({
         zIndex: "unset",
       },
       ".mosaic-window-title": {
-        fontSize: "12px",
+        fontSize: 12,
         lineHeight: "30px",
-        paddingLeft: "5px",
-        color: colors.GREY,
+        paddingLeft: 5,
       },
       ".mosaic-split": {
         background: "none !important",
         zIndex: 99,
 
         ".mosaic-split-line": {
-          boxShadow: `0 0 0 1px ${theme.palette.neutralLighter}`,
+          boxShadow: `0 0 0 1px ${palette.grey[palette.mode === "dark" ? "A200" : "A100"]}`,
         },
-        ":hover .mosaic-split-line": {
-          boxShadow: `0 0 0 1px ${theme.palette.neutralQuaternary}`,
+        "&:hover .mosaic-split-line": {
+          boxShadow: `0 0 0 1px ${palette.grey[palette.mode === "dark" ? "A200" : "A100"]}`,
         },
         "&.-row": {
           marginTop: 2,
@@ -173,40 +172,43 @@ const useStyles = makeStyles((theme) => ({
     },
 
     // leaflet GUI styling
-    ".leaflet-bar a": {
+    ".leaflet-bar": {
       userSelect: "none",
-      backgroundColor: theme.semanticColors.bodyStandoutBackground,
-      color: theme.semanticColors.buttonText,
-      borderBottomColor: theme.semanticColors.bodyDivider,
+      backgroundColor: palette.background.paper,
+      borderRadius: 4,
 
-      ":hover": {
-        backgroundColor: theme.semanticColors.bodyBackgroundHovered,
-        color: theme.semanticColors.buttonTextHovered,
-        borderBottomColor: theme.semanticColors.bodyDivider,
-      },
-      ":focus": {
-        color: theme.semanticColors.buttonTextPressed,
-      },
-      ":active": {
-        color: theme.semanticColors.buttonTextPressed,
+      a: {
+        lineHeight: 1.2,
+        backgroundColor: "transparent",
+        color: palette.text.primary,
+        borderBottomColor: palette.divider,
+
+        "&:hover": {
+          backgroundColor: palette.action.hover,
+          color: palette.text.primary,
+          borderBottomColor: palette.divider,
+        },
+        "&:focus": {
+          color: palette.text.primary,
+        },
+        "&:active": {
+          color: palette.text.primary,
+        },
       },
     },
     ".leaflet-bar a.leaflet-disabled": {
-      backgroundColor: theme.semanticColors.bodyBackground,
-      color: theme.semanticColors.disabledBodyText,
+      backgroundColor: palette.action.disabledBackground,
+      color: palette.text.disabled,
 
-      ":hover": {
-        backgroundColor: theme.semanticColors.bodyBackgroundHovered,
+      "&:hover": {
+        backgroundColor: palette.action.disabledBackground,
       },
     },
   },
 }));
 
 export default function CssBaseline(props: PropsWithChildren<unknown>): JSX.Element {
-  const styles = useStyles();
+  const { classes } = useStyles();
 
-  // styles scoped to our container
-  const className = styles.root;
-
-  return <div className={className}>{props.children}</div>;
+  return <div className={classes.root}>{props.children}</div>;
 }

packages/studio-base/src/components/Autocomplete.tsx
@@ -11,8 +11,8 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { Layer, makeStyles } from "@fluentui/react";
-import cx from "classnames";
+import { Layer } from "@fluentui/react";
+import { alpha, Paper, useTheme } from "@mui/material";
 import { Fzf, FzfResultItem } from "fzf";
 import { maxBy } from "lodash";
 import React, {
@@ -25,8 +25,9 @@ import React, {
 } from "react";
 import ReactAutocomplete from "react-autocomplete";
 import textMetrics from "text-metrics";
+import { makeStyles } from "tss-react/mui";
 
-import { colors, fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
+import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 // react-autocomplete tries to auto-scroll as soon as the menu is rendered, which is not compatible
 // with fluentui's Layer because the Layer takes a couple of react render cycles before elements are
@@ -46,14 +47,11 @@ function measureText(text: string): number {
 const ROW_HEIGHT = 24;
 const MAX_ITEMS = 200;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   root: {
-    borderRadius: 3,
     borderTopLeftRadius: 0,
-    boxShadow: theme.effects.elevation16,
     position: "fixed",
     overflow: "auto",
-    background: theme.semanticColors.menuBackground,
     zIndex: 1,
     marginLeft: -6,
   },
@@ -61,7 +59,7 @@ const useStyles = makeStyles((theme) => ({
     background: "transparent !important",
     borderRadius: 0,
     border: "none",
-    color: theme.semanticColors.inputText,
+    color: theme.palette.text.primary,
     flexGrow: 1,
     fontSize: "1rem",
     margin: 0,
@@ -70,36 +68,38 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: fonts.SANS_SERIF,
 
     "&.disabled, &[disabled]": {
-      color: theme.semanticColors.disabledText,
-      backgroundColor: theme.semanticColors.disabledBackground,
+      color: theme.palette.text.disabled,
     },
     "&:focus": {
       outline: "none",
     },
     "&::placeholder": {
-      color: theme.semanticColors.inputPlaceholderText,
+      color: theme.palette.text.secondary,
     },
   },
   inputError: {
-    color: `${theme.semanticColors.errorIcon} !important`,
+    color: `${theme.palette.error.main} !important`,
   },
   inputPlaceholder: {
-    color: theme.semanticColors.inputPlaceholderText,
+    color: theme.palette.text.secondary,
   },
   item: {
     padding: 6,
     cursor: "pointer",
     minHeight: ROW_HEIGHT,
-    lineHeight: ROW_HEIGHT - 10,
+    lineHeight: `${ROW_HEIGHT - 10}px`,
     overflowWrap: "break-word",
-    color: theme.semanticColors.menuItemText,
+    color: theme.palette.text.primary,
     whiteSpace: "pre",
   },
   itemSelected: {
-    backgroundColor: theme.semanticColors.menuItemBackgroundHovered,
+    backgroundColor: alpha(
+      theme.palette.primary.main,
+      theme.palette.action.selectedOpacity + theme.palette.action.hoverOpacity,
+    ),
   },
   itemHighlighted: {
-    backgroundColor: theme.semanticColors.menuItemBackgroundHovered,
+    backgroundColor: theme.palette.action.hover,
   },
 }));
 
@@ -176,12 +176,13 @@ function itemToFzfResult<T>(item: T): FzfResultItem<T> {
 }
 
 const HighlightChars = (props: { str: string; indices: Set<number> }) => {
+  const theme = useTheme();
   const chars = props.str.split("");
 
   const nodes = chars.map((char, i) => {
     if (props.indices.has(i)) {
       return (
-        <b key={i} style={{ color: colors.HIGHLIGHT }}>
+        <b key={i} style={{ color: theme.palette.info.main }}>
           {char}
         </b>
       );
@@ -202,7 +203,7 @@ export default React.forwardRef(function Autocomplete<T = unknown>(
   const ignoreBlur = useRef<boolean>(false);
 
   // Context
-  const classes = useStyles();
+  const { classes, cx } = useStyles();
 
   // State
   const [showAllItems, setShowAllItems] = useState<boolean>(false);
@@ -256,7 +257,7 @@ export default React.forwardRef(function Autocomplete<T = unknown>(
     [filterText, getItemText, items, sortWhenFiltering],
   );
 
-  const hasError = props.hasError ?? (autocompleteItems.length === 0 && value?.length);
+  const hasError = Boolean(props.hasError ?? (autocompleteItems.length === 0 && value?.length));
 
   const open = focused && autocompleteItems.length > 0;
   if (!open) {
@@ -480,7 +481,8 @@ export default React.forwardRef(function Autocomplete<T = unknown>(
         const maxHeight = `calc(100vh - 10px - ${style.top}px)`;
 
         return (
-          <div
+          <Paper
+            elevation={6}
             className={classes.root}
             key={
               autocompleteKey
@@ -510,7 +512,7 @@ export default React.forwardRef(function Autocomplete<T = unknown>(
             >
               {menuItemsToShow}
             </div>
-          </div>
+          </Paper>
         );
       }}
       // @ts-expect-error renderMenuWrapper added in the fork but we don't have typings for it

packages/studio-base/src/components/ColorPicker/index.tsx
@@ -88,7 +88,7 @@ export default function ColorPicker({
               tableHexCell: { width: "35%" },
               input: {
                 input: {
-                  fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
+                  fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero' !important`,
                 },
               },
             }}

packages/studio-base/src/components/GlobalCss.tsx
@@ -2,48 +2,54 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyles, useTheme } from "@mui/material";
 
 /** GlobalCss component configures html, body, and #root with theme elements */
-const GlobalCss = createGlobalStyle`
-html, body {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
+export default function GlobalCss(): JSX.Element {
+  const theme = useTheme();
 
-  // https://github.com/necolas/normalize.css/blob/master/normalize.css#L12
-  line-height: 1.15;
-}
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-body {
-  background: ${({ theme }) => theme.semanticColors.bodyBackground};
-  color: ${({ theme }) => theme.semanticColors.bodyText};
-  font: inherit;
-  font-family: ${({ theme }) => theme.fonts.small.fontFamily};
-  font-feature-settings: ${({ theme }) => theme.fonts.small.fontFeatureSettings};
-  font-size: ${({ theme }) => theme.fonts.small.fontSize};
-  font-weight: ${({ theme }) => theme.fonts.small.fontWeight};
+  return (
+    <GlobalStyles
+      styles={{
+        "html, body": {
+          boxSizing: "border-box",
+          margin: 0,
+          padding: 0,
+          height: "100%",
+          width: "100%",
 
-  // Prevent scroll "bouncing" since the app workspace is not scrollable. Allows individual
-  // scrollable elements to be scrolled without the whole page moving (even if they don't
-  // preventDefault on scroll events).
-  overscroll-behavior: none;
-}
-#root {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  flex: 1 1 100%;
-  outline: none;
-  overflow: hidden;
-  z-index: 0;
-}
-`;
+          // https://github.com/necolas/normalize.css/blob/master/normalize.css#L12
+          lineHeight: 1.15,
+        },
+        "*, *:before, *:after": {
+          boxSizing: "inherit",
+        },
+        body: {
+          background: theme.palette.background.default,
+          color: theme.palette.text.primary,
+          font: "inherit",
+          fontFamily: theme.typography.body2.fontFamily,
+          fontFeatureSettings: theme.typography.body2.fontFeatureSettings,
+          fontSize: theme.typography.body2.fontSize,
+          fontWeight: theme.typography.body2.fontWeight,
 
-export default GlobalCss;
+          // Prevent scroll "bouncing" since the app workspace is not scrollable. Allows individual
+          // scrollable elements to be scrolled without the whole page moving (even if they don't
+          // preventDefault on scroll events).
+          overscrollBehavior: "none",
+        },
+        "#root": {
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          position: "relative",
+          flex: "1 1 100%",
+          outline: "none",
+          overflow: "hidden",
+          zIndex: 0,
+        },
+      }}
+    />
+  );
+}

packages/studio-base/src/components/PlaybackControls/PlaybackTimeDisplayMethod.tsx
@@ -39,10 +39,10 @@ type PlaybackTimeDisplayMethodProps = {
 };
 
 const StyledTextField = muiStyled(TextField)<{ error?: boolean }>(({ error, theme }) => ({
-  fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
   borderRadius: theme.shape.borderRadius,
 
   ".MuiInputBase-input": {
+    fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero' !important`,
     minWidth: "20ch",
   },
   ".MuiIconButton-root": {

packages/studio-base/src/components/SettingsTreeEditor/inputs/ColorGradientInput.tsx
@@ -99,7 +99,7 @@ export function ColorGradientInput({
             tableHexCell: { width: "35%" },
             input: {
               input: {
-                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
+                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero' !important`,
               },
             },
           }}
@@ -126,7 +126,7 @@ export function ColorGradientInput({
             tableHexCell: { width: "35%" },
             input: {
               input: {
-                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
+                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero' !important`,
               },
             },
           }}

packages/studio-base/src/components/SettingsTreeEditor/inputs/ColorPickerInput.tsx
@@ -101,7 +101,7 @@ export function ColorPickerInput(props: ColorPickerInputProps): JSX.Element {
             tableHexCell: { width: "35%" },
             input: {
               input: {
-                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero'`,
+                fontFeatureSettings: `${fonts.SANS_SERIF_FEATURE_SETTINGS}, 'zero' !important`,
               },
             },
           }}

packages/studio-base/src/components/Sidebar/Badge.tsx
@@ -2,12 +2,12 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import { makeStyles } from "@fluentui/react";
 import { PropsWithChildren } from "react";
+import { makeStyles } from "tss-react/mui";
 
 const radius = 7;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles()((theme) => ({
   badge: {
     position: "absolute",
     bottom: -radius,
@@ -15,19 +15,19 @@ const useStyles = makeStyles((theme) => ({
     width: radius * 2,
     height: radius * 2,
     borderRadius: radius,
-    backgroundColor: theme.semanticColors.errorBackground,
-    color: "rgba(255, 255, 255, 0.8)",
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.error.contrastText,
     fontSize: 8,
     fontWeight: 700,
     fontFeatureSettings: "normal",
     letterSpacing: "-0.025em",
-    lineHeight: radius * 2,
+    lineHeight: `${radius * 2}px`,
     textAlign: "center",
   },
 }));
 
 export function Badge(props: PropsWithChildren<{ count?: number }>): JSX.Element {
-  const classes = useStyles();
+  const { classes } = useStyles();
   const { count } = props;
 
   return (

commit 4f18539f110f68adef5f7a7fe2b8b835bf14a2c2

Tweak mosaic drag overlay colors (#4218)
**User-Facing Changes**
Not worth calling out in release notes

**Description**
Fixes #4186, a color regression from https://github.com/foxglove/studio/pull/3987.

packages/studio-base/src/components/CssBaseline.tsx
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { alpha } from "@mui/material";
 import { PropsWithChildren } from "react";
 import { makeStyles } from "tss-react/mui";
 
@@ -67,10 +68,10 @@ const useStyles = makeStyles()(({ palette, typography }) => ({
 
         ".drop-target-container .drop-target": {
           backgroundColor: palette.action.hover,
-          border: `2px solid ${palette.divider}`,
+          border: `2px solid ${alpha(palette.divider, 0.5)}`,
         },
         ".drop-target-container .drop-target-hover": {
-          opacity: 0.3,
+          opacity: 1,
         },
       },
       ".mosaic-tile": {