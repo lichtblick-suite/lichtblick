lichtblick-suite/lichtblick

commit 3890c114022c6f3752d337d8a623fe07d565817c

MessagePipeline: replace createSelectableContext with zustand (#4407)
**User-Facing Changes**
None

**Description**
- Removes createSelectableContext/useContextSelector and replaces it
with a zustand store.
- Re-combines the pieces of state that had been factored out as
`usePlayerState` back into the top-level state, because with zustand all
our state needs to be in one store.
- The parts of state which can be consumed using useMessagePipeline
selectors have been moved under a `public` sub-object.

packages/studio-base/src/components/MessagePipeline/store.ts
@@ -1,0 +1,358 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { flatten } from "lodash";
+import { MutableRefObject } from "react";
+import shallowequal from "shallowequal";
+import { createStore, StoreApi } from "zustand";
+
+import { Condvar } from "@foxglove/den/async";
+import { MessageEvent } from "@foxglove/studio";
+import {
+  AdvertiseOptions,
+  Player,
+  PlayerCapabilities,
+  PlayerPresence,
+  PlayerState,
+  SubscribePayload,
+} from "@foxglove/studio-base/players/types";
+import { assertNever } from "@foxglove/studio-base/util/assertNever";
+
+import { FramePromise } from "./pauseFrameForPromise";
+import { MessagePipelineContext } from "./types";
+
+export function defaultPlayerState(): PlayerState {
+  return {
+    presence: PlayerPresence.NOT_PRESENT,
+    progress: {},
+    capabilities: [],
+    profile: undefined,
+    playerId: "",
+    activeData: undefined,
+  };
+}
+
+export type MessagePipelineInternalState = {
+  dispatch: (action: MessagePipelineStateAction) => void;
+
+  player?: Player;
+
+  /** used to keep track of whether we need to update public.startPlayback/playUntil/etc. */
+  lastCapabilities: string[];
+
+  subscriptionsById: Map<string, SubscribePayload[]>;
+  publishersById: { [key: string]: AdvertiseOptions[] };
+  subscriberIdsByTopic: Map<string, string[]>;
+  newTopicsBySubscriberId: Map<string, Set<string>>;
+  lastMessageEventByTopic: Map<string, MessageEvent<unknown>>;
+  /** Function to call when react render has completed with the latest state */
+  renderDone?: () => void;
+
+  /** Part of the state that is exposed to consumers via useMessagePipeline */
+  public: MessagePipelineContext;
+};
+
+type UpdateSubscriberAction = {
+  type: "update-subscriber";
+  id: string;
+  payloads: SubscribePayload[];
+};
+type UpdatePlayerStateAction = {
+  type: "update-player-state";
+  playerState: PlayerState;
+  renderDone?: () => void;
+};
+
+export type MessagePipelineStateAction =
+  | UpdateSubscriberAction
+  | UpdatePlayerStateAction
+  | { type: "set-player"; player: Player | undefined }
+  | { type: "set-publishers"; id: string; payloads: AdvertiseOptions[] };
+
+export function createMessagePipelineStore({
+  promisesToWaitForRef,
+  initialPlayer,
+}: {
+  promisesToWaitForRef: MutableRefObject<FramePromise[]>;
+  initialPlayer: Player | undefined;
+}): StoreApi<MessagePipelineInternalState> {
+  return createStore((set, get) => ({
+    player: initialPlayer,
+    dispatch(action) {
+      set((state) => reducer(state, action));
+    },
+    publishersById: {},
+    subscriptionsById: new Map(),
+    subscriberIdsByTopic: new Map(),
+    newTopicsBySubscriberId: new Map(),
+    lastMessageEventByTopic: new Map(),
+    lastCapabilities: [],
+
+    public: {
+      playerState: defaultPlayerState(),
+      messageEventsBySubscriberId: new Map(),
+      subscriptions: [],
+      publishers: [],
+      sortedTopics: [],
+      datatypes: new Map(),
+      setSubscriptions(id, payloads) {
+        get().dispatch({ type: "update-subscriber", id, payloads });
+      },
+      setPublishers(id, payloads) {
+        get().dispatch({ type: "set-publishers", id, payloads });
+      },
+      setParameter(key, value) {
+        get().player?.setParameter(key, value);
+      },
+      publish(payload) {
+        get().player?.publish(payload);
+      },
+      async callService(service, request) {
+        const player = get().player;
+        if (player) {
+          return await player.callService(service, request);
+        }
+        throw new Error("callService called when player is not present");
+      },
+      startPlayback: undefined,
+      playUntil: undefined,
+      pausePlayback: undefined,
+      setPlaybackSpeed: undefined,
+      seekPlayback: undefined,
+
+      pauseFrame(name: string) {
+        const condvar = new Condvar();
+        promisesToWaitForRef.current.push({ name, promise: condvar.wait() });
+        return () => {
+          condvar.notifyAll();
+        };
+      },
+    },
+  }));
+}
+// Update state with a subscriber. Any new topics for the subscriber are tracked in newTopicsBySubscriberId
+// to receive the last message on their newly subscribed topics.
+function updateSubscriberAction(
+  prevState: MessagePipelineInternalState,
+  action: UpdateSubscriberAction,
+): MessagePipelineInternalState {
+  const previousSubscriptionsById = prevState.subscriptionsById;
+  const newTopicsBySubscriberId = new Map(prevState.newTopicsBySubscriberId);
+
+  // Record any _new_ topics for this subscriber into newTopicsBySubscriberId
+  const newTopics = newTopicsBySubscriberId.get(action.id);
+  if (!newTopics) {
+    const actionTopics = action.payloads.map((sub) => sub.topic);
+    newTopicsBySubscriberId.set(action.id, new Set(actionTopics));
+  } else {
+    const previousSubscription = previousSubscriptionsById.get(action.id);
+    const prevTopics = new Set(previousSubscription?.map((sub) => sub.topic) ?? []);
+    for (const { topic: newTopic } of action.payloads) {
+      if (!prevTopics.has(newTopic)) {
+        newTopics.add(newTopic);
+      }
+    }
+  }
+
+  const newSubscriptionsById = new Map(previousSubscriptionsById);
+
+  if (action.payloads.length === 0) {
+    // When a subscription id has no topics we removed it from our map
+    newSubscriptionsById.delete(action.id);
+  } else {
+    newSubscriptionsById.set(action.id, action.payloads);
+  }
+
+  const subscriberIdsByTopic = new Map<string, string[]>();
+
+  const subscriptions: SubscribePayload[] = [];
+
+  // make a map of topics to subscriber ids
+  for (const [id, subs] of newSubscriptionsById) {
+    for (const subscription of subs) {
+      const topic = subscription.topic;
+
+      const ids = subscriberIdsByTopic.get(topic) ?? [];
+      ids.push(id);
+      subscriberIdsByTopic.set(topic, ids);
+      subscriptions.push(subscription);
+    }
+  }
+
+  return {
+    ...prevState,
+    subscriptionsById: newSubscriptionsById,
+    subscriberIdsByTopic,
+    newTopicsBySubscriberId,
+    public: {
+      ...prevState.public,
+      subscriptions,
+    },
+  };
+}
+// Update with a player state.
+// Put messages from the player state into messagesBySubscriberId. Any new topic subscribers, receive
+// the last message on a topic.
+function updatePlayerStateAction(
+  prevState: MessagePipelineInternalState,
+  action: UpdatePlayerStateAction,
+): MessagePipelineInternalState {
+  const messages = action.playerState.activeData?.messages;
+
+  const seenTopics = new Set<string>();
+
+  // We need a new set of message arrays for each subscriber since downstream users rely
+  // on object instance reference checks to determine if there are new messages
+  const messagesBySubscriberId = new Map<string, MessageEvent<unknown>[]>();
+
+  const subsById = prevState.subscriptionsById;
+  const subscriberIdsByTopic = prevState.subscriberIdsByTopic;
+
+  const lastMessageEventByTopic = prevState.lastMessageEventByTopic;
+  const newTopicsBySubscriberId = new Map(prevState.newTopicsBySubscriberId);
+
+  // Put messages into per-subscriber queues
+  if (messages && messages !== prevState.public.playerState.activeData?.messages) {
+    for (const messageEvent of messages) {
+      // Save the last message on every topic to send the last message
+      // to newly subscribed panels.
+      lastMessageEventByTopic.set(messageEvent.topic, messageEvent);
+
+      seenTopics.add(messageEvent.topic);
+      const ids = subscriberIdsByTopic.get(messageEvent.topic);
+      if (!ids) {
+        continue;
+      }
+
+      for (const id of ids) {
+        let subscriberMessageEvents = messagesBySubscriberId.get(id);
+        if (!subscriberMessageEvents) {
+          subscriberMessageEvents = [];
+          messagesBySubscriberId.set(id, subscriberMessageEvents);
+        }
+        subscriberMessageEvents.push(messageEvent);
+      }
+    }
+  }
+
+  // Inject the last message on a topic to all new subscribers of the topic
+  for (const id of subsById.keys()) {
+    const newTopics = newTopicsBySubscriberId.get(id);
+    if (!newTopics) {
+      continue;
+    }
+    for (const topic of newTopics) {
+      // If we had a message for this topic in the regular set of messages, we don't need to inject
+      // another message.
+      if (seenTopics.has(topic)) {
+        continue;
+      }
+      const msgEvent = lastMessageEventByTopic.get(topic);
+      if (msgEvent) {
+        const subscriberMessageEvents = messagesBySubscriberId.get(id) ?? [];
+        // the injected message is older than any new messages
+        subscriberMessageEvents.unshift(msgEvent);
+        messagesBySubscriberId.set(id, subscriberMessageEvents);
+      }
+    }
+    // We've processed all new subscriber topics into message queues
+    newTopics.clear();
+  }
+
+  const newPublicState = {
+    ...prevState.public,
+    playerState: action.playerState,
+    messageEventsBySubscriberId: messagesBySubscriberId,
+  };
+
+  const topics = action.playerState.activeData?.topics;
+  if (!shallowequal(topics, prevState.public.playerState.activeData?.topics)) {
+    newPublicState.sortedTopics = topics
+      ? [...topics].sort((a, b) => a.name.localeCompare(b.name))
+      : [];
+  }
+  if (
+    !shallowequal(
+      action.playerState.activeData?.datatypes,
+      prevState.public.playerState.activeData?.datatypes,
+    )
+  ) {
+    newPublicState.datatypes = action.playerState.activeData?.datatypes ?? new Map();
+  }
+
+  const capabilities = action.playerState.capabilities;
+  const player = prevState.player;
+  if (player && !shallowequal(capabilities, prevState.lastCapabilities)) {
+    newPublicState.startPlayback = capabilities.includes(PlayerCapabilities.playbackControl)
+      ? player.startPlayback?.bind(player)
+      : undefined;
+    newPublicState.playUntil = capabilities.includes(PlayerCapabilities.playbackControl)
+      ? player.playUntil?.bind(player)
+      : undefined;
+    newPublicState.pausePlayback = capabilities.includes(PlayerCapabilities.playbackControl)
+      ? player.pausePlayback?.bind(player)
+      : undefined;
+    newPublicState.setPlaybackSpeed = capabilities.includes(PlayerCapabilities.setSpeed)
+      ? player.setPlaybackSpeed?.bind(player)
+      : undefined;
+    newPublicState.seekPlayback = capabilities.includes(PlayerCapabilities.playbackControl)
+      ? player.seekPlayback?.bind(player)
+      : undefined;
+  }
+
+  return {
+    ...prevState,
+    newTopicsBySubscriberId,
+    renderDone: action.renderDone,
+    public: newPublicState,
+    lastCapabilities: capabilities,
+  };
+}
+
+export function reducer(
+  prevState: MessagePipelineInternalState,
+  action: MessagePipelineStateAction,
+): MessagePipelineInternalState {
+  switch (action.type) {
+    case "update-player-state":
+      return updatePlayerStateAction(prevState, action);
+    case "update-subscriber":
+      return updateSubscriberAction(prevState, action);
+
+    case "set-publishers": {
+      const newPublishersById = { ...prevState.publishersById, [action.id]: action.payloads };
+
+      return {
+        ...prevState,
+        publishersById: newPublishersById,
+        public: { ...prevState.public, publishers: flatten(Object.values(newPublishersById)) },
+      };
+    }
+
+    case "set-player":
+      if (action.player === prevState.player) {
+        return prevState;
+      }
+      return {
+        ...prevState,
+        player: action.player,
+        lastCapabilities: [],
+        public: {
+          ...prevState.public,
+          sortedTopics: [],
+          datatypes: new Map(),
+          startPlayback: undefined,
+          pausePlayback: undefined,
+          playUntil: undefined,
+          setPlaybackSpeed: undefined,
+          seekPlayback: undefined,
+        },
+      };
+  }
+
+  assertNever(
+    action,
+    `Unhandled message pipeline action type ${(action as MessagePipelineStateAction).type}`,
+  );
+}

packages/studio-base/src/components/DataSourceSidebar/TopicList.tsx
@@ -105,12 +105,9 @@ const StyledListItem = muiStyled(ListItem, { skipSx: true })(({ theme }) => ({
   // },
 }));
 
-const EMPTY_TOPICS: Topic[] = [];
-
 const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
 
-const selectTopics = (ctx: MessagePipelineContext) =>
-  ctx.playerState.activeData?.topics ?? EMPTY_TOPICS;
+const selectSortedTopics = (ctx: MessagePipelineContext) => ctx.sortedTopics;
 
 function TopicListItem({
   topic,
@@ -170,7 +167,7 @@ export function TopicList(): JSX.Element {
   const [filterText, setFilterText] = useState<string>("");
 
   const playerPresence = useMessagePipeline(selectPlayerPresence);
-  const topics = useMessagePipeline(selectTopics);
+  const topics = useMessagePipeline(selectSortedTopics);
 
   const filteredTopics: FzfResultItem<Topic>[] = useMemo(
     () =>

packages/studio-base/src/components/MessagePipeline/MockMessagePipelineProvider.tsx
@@ -11,18 +11,17 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
-import { flatten } from "lodash";
-import { useCallback, useMemo, useRef, useState } from "react";
-import { useLatest } from "react-use";
+import { omit } from "lodash";
+import { useEffect, useRef, useState } from "react";
+import shallowequal from "shallowequal";
+import { createStore } from "zustand";
 
-import { useShallowMemo } from "@foxglove/hooks";
 import { Time, isLessThan } from "@foxglove/rostime";
 import { ParameterValue } from "@foxglove/studio";
 import {
   AdvertiseOptions,
   MessageEvent,
   PlayerPresence,
-  PlayerState,
   PlayerStateActiveData,
   PlayerProblem,
   Progress,
@@ -33,16 +32,15 @@ import {
   TopicStats,
 } from "@foxglove/studio-base/players/types";
 import { RosDatatypes } from "@foxglove/studio-base/types/RosDatatypes";
-import naturalSort from "@foxglove/studio-base/util/naturalSort";
 
 import { ContextInternal } from "./index";
+import { MessagePipelineInternalState, MessagePipelineStateAction, reducer } from "./store";
 
 const NO_DATATYPES = new Map();
 
 function noop() {}
 
-export default function MockMessagePipelineProvider(props: {
-  children: React.ReactNode;
+type MockMessagePipelineProps = {
   presence?: PlayerPresence;
   topics?: Topic[];
   topicStats?: Map<string, TopicStats>;
@@ -69,7 +67,103 @@ export default function MockMessagePipelineProvider(props: {
   playerId?: string;
   progress?: Progress;
   urlState?: PlayerURLState;
-}): React.ReactElement {
+  /* eslint-enable react/no-unused-prop-types */
+};
+type MockMessagePipelineState = MessagePipelineInternalState & {
+  mockProps: MockMessagePipelineProps;
+  dispatch: (
+    action:
+      | MessagePipelineStateAction
+      | { type: "set-mock-props"; mockProps: MockMessagePipelineProps },
+  ) => void;
+};
+
+function getPublicState(
+  prevState: MockMessagePipelineState | undefined,
+  props: MockMessagePipelineProps,
+  dispatch: MockMessagePipelineState["dispatch"],
+): Omit<MessagePipelineInternalState["public"], "messageEventsBySubscriberId"> {
+  let startTime = prevState?.public.playerState.activeData?.startTime;
+  let currentTime = props.currentTime;
+  if (!currentTime) {
+    for (const message of props.messages ?? []) {
+      if (startTime == undefined || isLessThan(message.receiveTime, startTime)) {
+        startTime = message.receiveTime;
+      }
+      if (!currentTime || isLessThan(currentTime, message.receiveTime)) {
+        currentTime = message.receiveTime;
+      }
+    }
+  }
+
+  return {
+    playerState: {
+      presence: props.presence ?? PlayerPresence.PRESENT,
+      playerId: props.playerId ?? "1",
+      progress: props.progress ?? {},
+      capabilities: props.capabilities ?? prevState?.public.playerState.capabilities ?? [],
+      profile: props.profile,
+      problems: props.problems,
+      urlState: props.urlState,
+      activeData:
+        props.noActiveData === true
+          ? undefined
+          : {
+              messages: props.messages ?? [],
+              topics: props.topics ?? [],
+              topicStats: props.topicStats ?? new Map(),
+              datatypes: props.datatypes ?? NO_DATATYPES,
+              startTime: props.startTime ?? startTime ?? { sec: 100, nsec: 0 },
+              currentTime: currentTime ?? { sec: 100, nsec: 0 },
+              endTime: props.endTime ?? currentTime ?? { sec: 100, nsec: 0 },
+              isPlaying: props.isPlaying ?? false,
+              speed: 0.2,
+              lastSeekTime: 0,
+              totalBytesReceived: 0,
+              ...props.activeData,
+            },
+    },
+    subscriptions: [],
+    publishers: [],
+    sortedTopics:
+      props.topics === prevState?.mockProps.topics
+        ? prevState?.public.sortedTopics ?? []
+        : props.topics
+        ? [...props.topics].sort((a, b) => a.name.localeCompare(b.name))
+        : [],
+    datatypes: props.datatypes ?? NO_DATATYPES,
+    setSubscriptions:
+      (props.setSubscriptions === prevState?.mockProps.setSubscriptions
+        ? prevState?.public.setSubscriptions
+        : undefined) ??
+      ((id, payloads) => {
+        dispatch({ type: "update-subscriber", id, payloads });
+        props.setSubscriptions?.(id, payloads);
+      }),
+    setPublishers:
+      (props.setPublishers === prevState?.mockProps.setPublishers
+        ? prevState?.public.setPublishers
+        : undefined) ??
+      ((id, payloads) => {
+        dispatch({ type: "set-publishers", id, payloads });
+        props.setPublishers?.(id, payloads);
+      }),
+    setParameter: props.setParameter ?? noop,
+    publish: props.publish ?? noop,
+    callService: props.callService ?? (async () => {}),
+    startPlayback: props.startPlayback,
+    playUntil: noop,
+    pausePlayback: props.pausePlayback,
+    setPlaybackSpeed: noop,
+    seekPlayback: props.seekPlayback,
+
+    pauseFrame: props.pauseFrame ?? (() => noop),
+  };
+}
+
+export default function MockMessagePipelineProvider(
+  props: React.PropsWithChildren<MockMessagePipelineProps>,
+): React.ReactElement {
   const startTime = useRef<Time | undefined>();
   let currentTime = props.currentTime;
   if (!currentTime) {
@@ -83,119 +177,87 @@ export default function MockMessagePipelineProvider(props: {
     }
   }
 
-  // See comment for messageEventsBySubscriberId below on the purpose of this ref
-  const firstChangeRef = useRef<boolean>(false);
+  const [store] = useState(() =>
+    createStore<MockMessagePipelineState>((set) => {
+      const dispatch: MockMessagePipelineState["dispatch"] = (action) => {
+        if (action.type === "set-mock-props") {
+          set((state) => {
+            if (shallowequal(state.mockProps, action.mockProps)) {
+              return state;
+            }
+            const publicState = getPublicState(state, action.mockProps, state.dispatch);
+            const newState = reducer(state, {
+              type: "update-player-state",
+              playerState: publicState.playerState,
+            });
+            return {
+              ...newState,
+              mockProps: action.mockProps,
+              dispatch: state.dispatch,
+              public: {
+                ...publicState,
+                messageEventsBySubscriberId: newState.public.messageEventsBySubscriberId,
+              },
+            };
+          });
+        } else {
+          set((state) => {
+            // In the real pipeline, the messageEventsBySubscriberId only change
+            // on player listener callback - not on subscriber changes
+            //
+            // In tests, the first setSubscriptions call happens after we've already set props.messages
+            // So we have some special logic to detect the change of subscriptions
+            // and update messageEventsBySubscriberId.
+            const newState = reducer(state, action);
+            const messages = newState.public.playerState.activeData?.messages;
+            if (action.type === "update-subscriber" && messages && messages.length !== 0) {
+              let changed = false;
+              const messageEventsBySubscriberId = new Map<string, MessageEvent<unknown>[]>();
+              for (const [id, subs] of newState.subscriptionsById) {
+                const existingMsgs = newState.public.messageEventsBySubscriberId.get(id);
+                const newMsgs = messages.filter(
+                  (msg) => subs.find((sub) => sub.topic === msg.topic) != undefined,
+                );
+                if (existingMsgs && shallowequal(existingMsgs, newMsgs)) {
+                  messageEventsBySubscriberId.set(id, existingMsgs);
+                } else {
+                  messageEventsBySubscriberId.set(id, newMsgs);
+                  changed = true;
+                  continue;
+                }
+              }
 
-  const [allSubscriptions, setAllSubscriptions] = useState<{
-    [key: string]: SubscribePayload[];
-  }>({});
-  const flattenedSubscriptions: SubscribePayload[] = useMemo(
-    () => flatten(Object.values(allSubscriptions)),
-    [allSubscriptions],
-  );
-  const setSubscriptions = useCallback(
-    (id: string, subs: SubscribePayload[]) => {
-      setAllSubscriptions((sub) => ({ ...sub, [id]: subs }));
-      const setSubs = props.setSubscriptions;
-      setSubs?.(id, subs);
-      if (subs.length > 0) {
-        firstChangeRef.current = true;
-      }
-    },
-    [setAllSubscriptions, props.setSubscriptions],
-  );
-
-  const capabilities = useShallowMemo(props.capabilities ?? []);
-
-  const playerState = useMemo<PlayerState>(
-    () => ({
-      presence: props.presence ?? PlayerPresence.PRESENT,
-      playerId: props.playerId ?? "1",
-      progress: props.progress ?? {},
-      capabilities,
-      profile: props.profile,
-      problems: props.problems,
-      urlState: props.urlState,
-      activeData:
-        props.noActiveData === true
-          ? undefined
-          : {
-              messages: props.messages ?? [],
-              topics: props.topics ?? [],
-              topicStats: props.topicStats ?? new Map(),
-              datatypes: props.datatypes ?? NO_DATATYPES,
-              startTime: props.startTime ?? startTime.current ?? { sec: 100, nsec: 0 },
-              currentTime: currentTime ?? { sec: 100, nsec: 0 },
-              endTime: props.endTime ?? currentTime ?? { sec: 100, nsec: 0 },
-              isPlaying: props.isPlaying ?? false,
-              speed: 0.2,
-              lastSeekTime: 0,
-              totalBytesReceived: 0,
-              ...props.activeData,
-            },
+              if (changed) {
+                newState.public.messageEventsBySubscriberId = messageEventsBySubscriberId;
+              }
+              return { ...newState, dispatch: state.dispatch };
+            }
+            return { ...newState, dispatch: state.dispatch };
+          });
+        }
+      };
+      const initialPublicState = getPublicState(undefined, props, dispatch);
+      return {
+        mockProps: omit(props, "children"),
+        player: undefined,
+        dispatch,
+        publishersById: {},
+        subscriptionsById: new Map(),
+        subscriberIdsByTopic: new Map(),
+        newTopicsBySubscriberId: new Map(),
+        lastMessageEventByTopic: new Map(),
+        lastCapabilities: initialPublicState.playerState.capabilities,
+        public: {
+          ...initialPublicState,
+          messageEventsBySubscriberId: new Map(),
+        },
+      };
     }),
-    [
-      props.presence,
-      props.playerId,
-      props.progress,
-      props.profile,
-      props.problems,
-      props.urlState,
-      props.noActiveData,
-      props.messages,
-      props.topics,
-      props.topicStats,
-      props.datatypes,
-      props.startTime,
-      props.endTime,
-      props.isPlaying,
-      props.activeData,
-      capabilities,
-      currentTime,
-    ],
   );
 
-  // In the real pipeline, the messageEventsBySubscriberId only change
-  // on player listener callback - not on subscriber changes
-  //
-  // In tests, the first setSubscriptions call happens after we've already set props.messages
-  // So we have some special logic to detect the _first_ change of subscriptions
-  // and update messageEventsBySubscriberId.
-  const latestAllSubs = useLatest(allSubscriptions);
-  const firstChange = firstChangeRef.current;
-  const messageEventsBySubscriberId = useMemo(() => {
-    void firstChange;
-    return new Map(
-      Object.entries(latestAllSubs.current).map(([id, payloads]) => [
-        id,
-        props.messages?.filter((msg) => payloads.find((payload) => payload.topic === msg.topic)) ??
-          [],
-      ]),
-    );
-  }, [firstChange, props.messages, latestAllSubs]);
+  useEffect(() => {
+    store.getState().dispatch({ type: "set-mock-props", mockProps: omit(props, "children") });
+  }, [props, store]);
 
-  return (
-    <ContextInternal.Provider
-      value={{
-        playerState,
-        sortedTopics: (props.topics ?? []).sort(naturalSort("name")),
-        datatypes: props.datatypes ?? NO_DATATYPES,
-        subscriptions: flattenedSubscriptions,
-        publishers: [],
-        messageEventsBySubscriberId,
-        setSubscriptions,
-        setPublishers: props.setPublishers ?? noop,
-        setParameter: props.setParameter ?? noop,
-        publish: props.publish ?? noop,
-        callService: props.callService ?? (async () => await Promise.reject()),
-        startPlayback: props.startPlayback ?? noop,
-        pausePlayback: props.pausePlayback ?? noop,
-        setPlaybackSpeed: noop,
-        seekPlayback: props.seekPlayback ?? noop,
-        pauseFrame: props.pauseFrame ?? (() => noop),
-      }}
-    >
-      {props.children}
-    </ContextInternal.Provider>
-  );
+  return <ContextInternal.Provider value={store}>{props.children}</ContextInternal.Provider>;
 }

commit 0ce42eea75c35faeae01851702742c8b0176b2bd

Fix shallowequal in MessagePipeline topics/datatypes comparison (#4420)
**User-Facing Changes**
None

**Description**
Fixes an issue where datatypes from a bag file would not be vended out
to panels. (regression from #4407)
topics is `Topic[]` and datatypes is a `Map`, neither of which are
really appropriate for `shallowequal`. Just use object identity.

packages/studio-base/src/components/MessagePipeline/store.ts
@@ -267,16 +267,13 @@ function updatePlayerStateAction(
   };
 
   const topics = action.playerState.activeData?.topics;
-  if (!shallowequal(topics, prevState.public.playerState.activeData?.topics)) {
+  if (topics !== prevState.public.playerState.activeData?.topics) {
     newPublicState.sortedTopics = topics
       ? [...topics].sort((a, b) => a.name.localeCompare(b.name))
       : [];
   }
   if (
-    !shallowequal(
-      action.playerState.activeData?.datatypes,
-      prevState.public.playerState.activeData?.datatypes,
-    )
+    action.playerState.activeData?.datatypes !== prevState.public.playerState.activeData?.datatypes
   ) {
     newPublicState.datatypes = action.playerState.activeData?.datatypes ?? new Map();
   }