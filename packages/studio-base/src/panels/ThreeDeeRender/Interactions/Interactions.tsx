lichtblick-suite/lichtblick

commit 36347564e9f841f2e019e5b40664f483f8932870

Object inspector summaries for quaternions and colors (#5045)
**User-Facing Changes**
- Object inspectors in Raw Messages and 3D show summaries for Quaternion
and ColorRGBA

**Description**
Summarize two common types, Quaternion and ColorRGBA

<img width="485" alt="Screenshot 2022-12-21 at 6 49 26 PM"
src="https://user-images.githubusercontent.com/195374/209044747-a159fad8-2683-4440-99d4-5f589b8fed0e.png">

packages/studio-base/src/panels/ThreeDeeRender/Interactions/ObjectDetails.tsx
@@ -12,18 +12,16 @@
 //   You may not use this file except in compliance with the License.
 
 import { first, omit } from "lodash";
-import { ReactNode } from "react";
 import Tree from "react-json-tree";
 
-import { isTypicalFilterName } from "@foxglove/studio-base/components/MessagePathSyntax/isTypicalFilterName";
+import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import Stack from "@foxglove/studio-base/components/Stack";
+import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import { RosValue } from "@foxglove/studio-base/players/types";
-import { format, formatDuration } from "@foxglove/studio-base/util/formatTime";
+import { getItemString } from "@foxglove/studio-base/util/getItemString";
 import { useJsonTreeTheme } from "@foxglove/studio-base/util/globalConstants";
 
 import { InteractionData } from "./types";
-
-const DURATION_20_YEARS_SEC = 20 * 365 * 24 * 60 * 60;
 
 type Props = {
   readonly interactionData?: InteractionData;
@@ -45,6 +43,8 @@ function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element 
   // We need a plain object to sort the keys and omit interaction data
   const plainObject = maybePlainObject(selectedObject);
   const originalObject = omit(plainObject as Record<string, unknown>, "interactionData");
+
+  const [timezone] = useAppConfigurationValue<string>(AppSetting.TIMEZONE);
 
   if (topic.length === 0) {
     // show the original object directly if there is no interaction data
@@ -70,7 +70,9 @@ function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element 
         invertTheme={false}
         theme={{ ...jsonTreeTheme, tree: { margin: 0, whiteSpace: "pre-line" } }}
         hideRoot
-        getItemString={getItemString}
+        getItemString={(nodeType, data, itemType, itemString, keyPath) =>
+          getItemString(nodeType, data, itemType, itemString, keyPath, timezone)
+        }
         postprocessValue={maybePlainObject}
         labelRenderer={(markerKeyPath, _p1, _p2, _hasChildren) => {
           const label = first(markerKeyPath);
@@ -81,81 +83,6 @@ function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element 
         }}
       />
     </Stack>
-  );
-}
-
-function getArrow(x: number, y: number) {
-  if (x === 0 && y === 0) {
-    return;
-  }
-  return (
-    <span style={{ transform: `rotate(${Math.atan2(-y, x)}rad)`, display: "inline-block" }}>→</span>
-  );
-}
-
-function getItemString(
-  _nodeType: string,
-  data: unknown,
-  itemType: ReactNode,
-  itemString: string,
-  _keyPath: (string | number)[],
-  timezone?: string,
-): ReactNode {
-  if (typeof data !== "object" || data == undefined) {
-    return (
-      <span>
-        {itemType} {itemString}
-      </span>
-    );
-  }
-
-  const keys = Object.keys(data);
-  if (keys.length === 2) {
-    const { sec, nsec } = data as { sec?: number; nsec?: number };
-    if (typeof sec === "number" && typeof nsec === "number") {
-      // Values "too small" to be absolute epoch-based times are probably relative durations.
-      return sec < DURATION_20_YEARS_SEC ? (
-        formatDuration({ sec, nsec })
-      ) : (
-        <span>{format({ sec, nsec }, timezone)}</span>
-      );
-    }
-  }
-
-  // for vectors/points display length
-  if (keys.length === 2) {
-    const { x, y } = data as { x?: unknown; y?: unknown };
-    if (typeof x === "number" && typeof y === "number") {
-      const length = Math.sqrt(x * x + y * y);
-      return (
-        <span>
-          norm = {length.toFixed(2)} {getArrow(x, y)}
-        </span>
-      );
-    }
-  }
-
-  if (keys.length === 3) {
-    const { x, y, z } = data as { x?: unknown; y?: unknown; z?: unknown };
-    if (typeof x === "number" && typeof y === "number" && typeof z === "number") {
-      const length = Math.sqrt(x * x + y * y + z * z);
-      return (
-        <span>
-          norm = {length.toFixed(2)} {z === 0 ? getArrow(x, y) : undefined}
-        </span>
-      );
-    }
-  }
-
-  // Surface typically-used keys directly in the object summary so the user doesn't have to expand it.
-  const filterKeys = keys
-    .filter(isTypicalFilterName)
-    .map((key) => `${key}: ${(data as { [key: string]: unknown })[key]}`)
-    .join(", ");
-  return (
-    <span>
-      {itemType} {filterKeys.length > 0 ? filterKeys : itemString}
-    </span>
   );
 }
 

packages/studio-base/src/components/JsonTree/useGetItemStringWithTimezone.tsx
@@ -14,20 +14,8 @@
 import { ReactNode, useCallback } from "react";
 
 import { AppSetting } from "@foxglove/studio-base/AppSetting";
-import { isTypicalFilterName } from "@foxglove/studio-base/components/MessagePathSyntax/isTypicalFilterName";
 import { useAppConfigurationValue } from "@foxglove/studio-base/hooks/useAppConfigurationValue";
-import { format, formatDuration } from "@foxglove/studio-base/util/formatTime";
-
-const DURATION_20_YEARS_SEC = 20 * 365 * 24 * 60 * 60;
-
-function getArrow(x: number, y: number) {
-  if (x === 0 && y === 0) {
-    return;
-  }
-  return (
-    <span style={{ transform: `rotate(${Math.atan2(-y, x)}rad)`, display: "inline-block" }}>→</span>
-  );
-}
+import { getItemString } from "@foxglove/studio-base/util/getItemString";
 
 export default function useGetItemStringWithTimezone(): (
   type: string,
@@ -38,72 +26,7 @@ export default function useGetItemStringWithTimezone(): (
   const [timezone] = useAppConfigurationValue<string>(AppSetting.TIMEZONE);
   return useCallback(
     (type: string, data: unknown, itemType: ReactNode, itemString: string) =>
-      getItemString(type, data, itemType, itemString, timezone),
+      getItemString(type, data, itemType, itemString, [], timezone),
     [timezone],
-  );
-}
-
-function getItemString(
-  _type: string,
-  data: unknown,
-  itemType: ReactNode,
-  itemString: string,
-  timezone?: string,
-): ReactNode {
-  if (typeof data !== "object" || data == undefined) {
-    return (
-      <span>
-        {itemType} {itemString}
-      </span>
-    );
-  }
-
-  const keys = Object.keys(data);
-  if (keys.length === 2) {
-    const { sec, nsec } = data as { sec?: number; nsec?: number };
-    if (typeof sec === "number" && typeof nsec === "number") {
-      // Values "too small" to be absolute epoch-based times are probably relative durations.
-      return sec < DURATION_20_YEARS_SEC ? (
-        formatDuration({ sec, nsec })
-      ) : (
-        <span>{format({ sec, nsec }, timezone)}</span>
-      );
-    }
-  }
-
-  // for vectors/points display length
-  if (keys.length === 2) {
-    const { x, y } = data as { x?: unknown; y?: unknown };
-    if (typeof x === "number" && typeof y === "number") {
-      const length = Math.sqrt(x * x + y * y);
-      return (
-        <span>
-          norm = {length.toFixed(2)} {getArrow(x, y)}
-        </span>
-      );
-    }
-  }
-
-  if (keys.length === 3) {
-    const { x, y, z } = data as { x?: unknown; y?: unknown; z?: unknown };
-    if (typeof x === "number" && typeof y === "number" && typeof z === "number") {
-      const length = Math.sqrt(x * x + y * y + z * z);
-      return (
-        <span>
-          norm = {length.toFixed(2)} {z === 0 ? getArrow(x, y) : undefined}
-        </span>
-      );
-    }
-  }
-
-  // Surface typically-used keys directly in the object summary so the user doesn't have to expand it.
-  const filterKeys = keys
-    .filter(isTypicalFilterName)
-    .map((key) => `${key}: ${(data as { [key: string]: unknown })[key]}`)
-    .join(", ");
-  return (
-    <span>
-      {itemType} {filterKeys.length > 0 ? filterKeys : itemString}
-    </span>
   );
 }

packages/studio-base/src/panels/ThreeDeeRender/Interactions/Interaction.stories.tsx
@@ -31,6 +31,12 @@ const markerObject = {
     y: 2,
     z: 4,
   },
+  orientation: {
+    x: 0,
+    y: 0,
+    z: Math.sin(Math.PI / 8),
+    w: Math.cos(Math.PI / 8),
+  },
   color: {
     r: 1,
     g: 0.1,

packages/studio-base/src/util/getItemString.tsx
@@ -1,0 +1,115 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { ReactNode } from "react";
+import tinycolor from "tinycolor2";
+
+import { isTypicalFilterName } from "@foxglove/studio-base/components/MessagePathSyntax/isTypicalFilterName";
+import { format, formatDuration } from "@foxglove/studio-base/util/formatTime";
+import { quatToEuler } from "@foxglove/studio-base/util/quatToEuler";
+
+const DURATION_20_YEARS_SEC = 20 * 365 * 24 * 60 * 60;
+
+export function getItemString(
+  _nodeType: string,
+  data: unknown,
+  itemType: ReactNode,
+  itemString: string,
+  _keyPath: (string | number)[],
+  timezone?: string,
+): ReactNode {
+  if (typeof data !== "object" || data == undefined) {
+    return (
+      <span>
+        {itemType} {itemString}
+      </span>
+    );
+  }
+
+  const keys = Object.keys(data);
+  if (keys.length === 2) {
+    const { sec, nsec } = data as { sec?: number; nsec?: number };
+    if (typeof sec === "number" && typeof nsec === "number") {
+      // Values "too small" to be absolute epoch-based times are probably relative durations.
+      return sec < DURATION_20_YEARS_SEC ? (
+        formatDuration({ sec, nsec })
+      ) : (
+        <span>{format({ sec, nsec }, timezone)}</span>
+      );
+    }
+  }
+
+  // for vectors/points display length
+  if (keys.length === 2) {
+    const { x, y } = data as { x?: unknown; y?: unknown };
+    if (typeof x === "number" && typeof y === "number") {
+      const length = Math.sqrt(x * x + y * y);
+      return (
+        <span>
+          norm = {length.toFixed(2)} {getArrow(x, y)}
+        </span>
+      );
+    }
+  } else if (keys.length === 3) {
+    const { x, y, z } = data as { x?: unknown; y?: unknown; z?: unknown };
+    if (typeof x === "number" && typeof y === "number" && typeof z === "number") {
+      const length = Math.sqrt(x * x + y * y + z * z);
+      return (
+        <span>
+          norm = {length.toFixed(2)} {z === 0 ? getArrow(x, y) : undefined}
+        </span>
+      );
+    }
+  } else if (keys.length === 4) {
+    const { x, y, z, w } = data as { x?: unknown; y?: unknown; z?: unknown; w?: unknown };
+    if (
+      typeof x === "number" &&
+      typeof y === "number" &&
+      typeof z === "number" &&
+      typeof w === "number"
+    ) {
+      const [roll, pitch, yaw] = quatToEuler(x, y, z, w);
+      return (
+        <span>
+          rpy = [{round(roll)}, {round(pitch)}, {round(yaw)}]
+        </span>
+      );
+    }
+
+    const { r, g, b, a } = data as { r?: unknown; g?: unknown; b?: unknown; a?: unknown };
+    if (
+      typeof r === "number" &&
+      typeof g === "number" &&
+      typeof b === "number" &&
+      typeof a === "number"
+    ) {
+      // print the color as hex
+      return <span>{tinycolor({ r: r * 255, g: g * 255, b: b * 255, a }).toHex8String()}</span>;
+    }
+  }
+
+  // Surface typically-used keys directly in the object summary so the user doesn't have to expand it.
+  const filterKeys = keys
+    .filter(isTypicalFilterName)
+    .map((key) => `${key}: ${(data as { [key: string]: unknown })[key]}`)
+    .join(", ");
+  return (
+    <span>
+      {itemType} {filterKeys.length > 0 ? filterKeys : itemString}
+    </span>
+  );
+}
+
+function getArrow(x: number, y: number) {
+  if (x === 0 && y === 0) {
+    return;
+  }
+  return (
+    <span style={{ transform: `rotate(${Math.atan2(-y, x)}rad)`, display: "inline-block" }}>→</span>
+  );
+}
+
+function round(x: number, precision = 2): number {
+  return Number(x.toFixed(precision));
+}

packages/studio-base/src/util/quatToEuler.ts
@@ -1,0 +1,26 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import * as THREE from "three";
+
+const tempQuaternion = new THREE.Quaternion();
+const tempEuler = new THREE.Euler();
+
+/**
+ * Convert a quaternion to roll-pitch-yaw Euler angles.
+ */
+export function quatToEuler(
+  x: number,
+  y: number,
+  z: number,
+  w: number,
+): [roll: number, pitch: number, yaw: number] {
+  tempQuaternion.set(x, y, z, w);
+  tempEuler.setFromQuaternion(tempQuaternion, "XYZ");
+  return [
+    THREE.MathUtils.radToDeg(tempEuler.x),
+    THREE.MathUtils.radToDeg(tempEuler.y),
+    THREE.MathUtils.radToDeg(tempEuler.z),
+  ];
+}

commit 25b7e68ce992c5cb0c0299c0a92fcc3aa482d2f0

Fix 3D panel crash on selection (#5427)
useAppConfigurationValue cannot be used in the 3D panel because it is
not inside of an AppConfigurationContext provider. Instead use the
subscribeAppSettings extension API to get the time zone.

Fixes #5426 (crash caused by #5045)
Fixes FG-2171

packages/studio-base/src/panels/ThreeDeeRender/Interactions/Interactions.tsx
@@ -42,6 +42,7 @@ type Props = {
   setInteractionsTabType: (arg0?: TabType) => void;
   addPanel: LayoutActions["addPanel"];
   selectedObject?: SelectionObject;
+  timezone: string | undefined;
 };
 
 const InteractionsBaseComponent = React.memo<Props>(function InteractionsBaseComponent({
@@ -49,6 +50,7 @@ const InteractionsBaseComponent = React.memo<Props>(function InteractionsBaseCom
   selectedObject,
   interactionsTabType,
   setInteractionsTabType,
+  timezone,
 }: Props) {
   const selectedInteractionData = selectedObject?.object.interactionData;
   const originalMessage = selectedInteractionData?.originalMessage;
@@ -68,10 +70,15 @@ const InteractionsBaseComponent = React.memo<Props>(function InteractionsBaseCom
               {selectedInteractionData.topic && (
                 <TopicLink addPanel={addPanel} topic={selectedInteractionData.topic} />
               )}
-              {instanceDetails ? <ObjectDetails selectedObject={instanceDetails} /> : <></>}
+              {instanceDetails ? (
+                <ObjectDetails selectedObject={instanceDetails} timezone={timezone} />
+              ) : (
+                <></>
+              )}
               <ObjectDetails
                 selectedObject={originalMessage}
                 interactionData={selectedInteractionData}
+                timezone={timezone}
               />
             </>
           ) : (

packages/studio-base/src/panels/ThreeDeeRender/Interactions/ObjectDetails.tsx
@@ -14,9 +14,7 @@
 import { first, omit } from "lodash";
 import Tree from "react-json-tree";
 
-import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import Stack from "@foxglove/studio-base/components/Stack";
-import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import { RosValue } from "@foxglove/studio-base/players/types";
 import { getItemString } from "@foxglove/studio-base/util/getItemString";
 import { useJsonTreeTheme } from "@foxglove/studio-base/util/globalConstants";
@@ -26,6 +24,7 @@ import { InteractionData } from "./types";
 type Props = {
   readonly interactionData?: InteractionData;
   readonly selectedObject?: RosValue;
+  readonly timezone: string | undefined;
 };
 
 function maybePlainObject(rawVal: unknown) {
@@ -35,7 +34,7 @@ function maybePlainObject(rawVal: unknown) {
   return rawVal;
 }
 
-function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element {
+function ObjectDetails({ interactionData, selectedObject, timezone }: Props): JSX.Element {
   const jsonTreeTheme = useJsonTreeTheme();
   const topic = interactionData?.topic ?? "";
 
@@ -43,8 +42,6 @@ function ObjectDetails({ interactionData, selectedObject }: Props): JSX.Element 
   // We need a plain object to sort the keys and omit interaction data
   const plainObject = maybePlainObject(selectedObject);
   const originalObject = omit(plainObject as Record<string, unknown>, "interactionData");
-
-  const [timezone] = useAppConfigurationValue<string>(AppSetting.TIMEZONE);
 
   if (topic.length === 0) {
     // show the original object directly if there is no interaction data

packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -34,6 +34,7 @@ import {
   Topic,
   VariableValue,
 } from "@foxglove/studio";
+import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import PublishGoalIcon from "@foxglove/studio-base/components/PublishGoalIcon";
 import PublishPointIcon from "@foxglove/studio-base/components/PublishPointIcon";
 import PublishPoseEstimateIcon from "@foxglove/studio-base/components/PublishPoseEstimateIcon";
@@ -127,6 +128,7 @@ function RendererOverlay(props: {
   publishClickType: PublishClickType;
   onChangePublishClickType: (_: PublishClickType) => void;
   onClickPublish: () => void;
+  timezone: string | undefined;
 }): JSX.Element {
   const { classes } = useStyles();
   const [clickedPosition, setClickedPosition] = useState<{ clientX: number; clientY: number }>({
@@ -251,6 +253,7 @@ function RendererOverlay(props: {
           selectedObject={selectedObject}
           interactionsTabType={interactionsTabType}
           setInteractionsTabType={setInteractionsTabType}
+          timezone={props.timezone}
         />
         <Paper square={false} elevation={4} style={{ display: "flex", flexDirection: "column" }}>
           <IconButton
@@ -435,6 +438,7 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
   }, [canvas, configRef, config.scene.transforms?.enablePreloading]);
 
   const [colorScheme, setColorScheme] = useState<"dark" | "light" | undefined>();
+  const [timezone, setTimezone] = useState<string | undefined>();
   const [topics, setTopics] = useState<ReadonlyArray<Topic> | undefined>();
   const [parameters, setParameters] = useState<ReadonlyMap<string, ParameterValue> | undefined>();
   const [variables, setVariables] = useState<ReadonlyMap<string, VariableValue> | undefined>();
@@ -599,6 +603,10 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
 
         // Keep UI elements and the renderer aware of the current color scheme
         setColorScheme(renderState.colorScheme);
+        if (renderState.appSettings) {
+          const tz = renderState.appSettings.get(AppSetting.TIMEZONE);
+          setTimezone(typeof tz === "string" ? tz : undefined);
+        }
 
         // We may have new topics - since we are also watching for messages in
         // the current frame, topics may not have changed
@@ -631,6 +639,8 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
     context.watch("sharedPanelState");
     context.watch("variables");
     context.watch("topics");
+    context.watch("appSettings");
+    context.subscribeAppSettings([AppSetting.TIMEZONE]);
   }, [context, renderer]);
 
   // Build a list of topics to subscribe to
@@ -1064,6 +1074,7 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
               renderer?.publishClickTool.setPublishClickType(type);
               renderer?.publishClickTool.start();
             }}
+            timezone={timezone}
           />
         </RendererContext.Provider>
       </div>