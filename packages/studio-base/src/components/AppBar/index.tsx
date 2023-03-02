lichtblick-suite/lichtblick

commit bf2d6e56b9db496fdf5e82ce8e6796fa3b5345a4

Move add panel list into top bar (#5405)
**User-Facing Changes**
None

**Description**
Moves Add Panel List to top nav behind feature flag

---------

Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>
Co-authored-by: Miles Egan <miles@foxglove.dev>

packages/studio-base/src/components/AppBar/index.tsx
@@ -7,14 +7,18 @@ import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CloseIcon from "@mui/icons-material/Close";
 import FilterNoneIcon from "@mui/icons-material/FilterNone";
 import MinimizeIcon from "@mui/icons-material/Minimize";
-import { AppBar as MuiAppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import { MouseEvent, useCallback, useState } from "react";
+import { AppBar as MuiAppBar, Button, IconButton, Toolbar } from "@mui/material";
+import { useCallback, useState } from "react";
 import { makeStyles } from "tss-react/mui";
 
 import { AppSetting } from "@foxglove/studio-base/AppSetting";
 import { FoxgloveLogo } from "@foxglove/studio-base/components/FoxgloveLogo";
 import { MemoryUseIndicator } from "@foxglove/studio-base/components/MemoryUseIndicator";
 import { useAnalytics } from "@foxglove/studio-base/context/AnalyticsContext";
+import {
+  LayoutState,
+  useCurrentLayoutSelector,
+} from "@foxglove/studio-base/context/CurrentLayoutContext";
 import {
   CurrentUser,
   useCurrentUserType,
@@ -25,14 +29,15 @@ import { useAppConfigurationValue } from "@foxglove/studio-base/hooks";
 import useNativeAppMenuEvent from "@foxglove/studio-base/hooks/useNativeAppMenuEvent";
 import { AppEvent } from "@foxglove/studio-base/services/IAnalytics";
 
+import { AddPanelIconButton, AddPanelMenu } from "./AddPanel";
 import { DataSource } from "./DataSource";
 import { HelpIconButton, HelpMenu } from "./Help";
 import { PreferencesDialog, PreferencesIconButton } from "./Preferences";
 import { UserIconButton, UserMenu } from "./User";
 import {
-  APP_BAR_HEIGHT,
   APP_BAR_BACKGROUND_COLOR,
   APP_BAR_FOREGROUND_COLOR,
+  APP_BAR_HEIGHT,
   APP_BAR_PRIMARY_COLOR,
 } from "./constants";
 
@@ -76,7 +81,7 @@ const useStyles = makeStyles<{ leftInset?: number; debugDragRegion?: boolean }>(
         display: "flex",
         flex: 1,
         alignItems: "center",
-        gap: theme.spacing(0.5),
+        gap: theme.spacing(1),
 
         [theme.breakpoints.up("sm")]: {
           marginInlineStart: theme.spacing(-2),
@@ -99,6 +104,12 @@ const useStyles = makeStyles<{ leftInset?: number; debugDragRegion?: boolean }>(
           marginInlineEnd: theme.spacing(-2),
         },
       },
+      endInner: {
+        display: "flex",
+        alignItems: "center",
+        gap: theme.spacing(1),
+        ...NOT_DRAGGABLE_STYLE, // make buttons clickable for desktop app
+      },
       button: {
         backgroundColor: APP_BAR_PRIMARY_COLOR,
 
@@ -110,12 +121,6 @@ const useStyles = makeStyles<{ leftInset?: number; debugDragRegion?: boolean }>(
       },
       iconButton: {
         padding: theme.spacing(0.375),
-      },
-      endInner: {
-        display: "flex",
-        alignItems: "center",
-        gap: theme.spacing(1),
-        ...NOT_DRAGGABLE_STYLE, // make buttons clickable for desktop app
       },
       noDrag: {
         ...NOT_DRAGGABLE_STYLE, // make buttons clickable for desktop app
@@ -148,6 +153,8 @@ type AppBarProps = CustomWindowControlsProps & {
   onSelectDataSourceAction: () => void;
 };
 
+const selectedLayoutIdSelector = (state: LayoutState) => state.selectedLayout?.id;
+
 export function AppBar(props: AppBarProps): JSX.Element {
   const {
     currentUser,
@@ -171,37 +178,19 @@ export function AppBar(props: AppBarProps): JSX.Element {
     AppSetting.ENABLE_MEMORY_USE_INDICATOR,
   );
 
+  const selectedLayoutId = useCurrentLayoutSelector(selectedLayoutIdSelector);
   const supportsAccountSettings = signIn != undefined;
 
   const { rightSidebarOpen, setRightSidebarOpen } = useWorkspace();
 
   const [helpAnchorEl, setHelpAnchorEl] = useState<undefined | HTMLElement>(undefined);
   const [userAnchorEl, setUserAnchorEl] = useState<undefined | HTMLElement>(undefined);
+  const [panelAnchorEl, setPanelAnchorEl] = useState<undefined | HTMLElement>(undefined);
   const [prefsDialogOpen, setPrefsDialogOpen] = useState(false);
 
   const helpMenuOpen = Boolean(helpAnchorEl);
   const userMenuOpen = Boolean(userAnchorEl);
-
-  const handleHelpClick = (event: MouseEvent<HTMLElement>) => {
-    setHelpAnchorEl(event.currentTarget);
-  };
-  const handleHelpClose = () => {
-    setHelpAnchorEl(undefined);
-  };
-
-  const handleUserMenuClick = (event: MouseEvent<HTMLElement>) => {
-    setUserAnchorEl(event.currentTarget);
-  };
-  const handleUserClose = () => {
-    setUserAnchorEl(undefined);
-  };
-
-  const openPreferences = () => {
-    setPrefsDialogOpen(true);
-  };
-  const closePreferences = () => {
-    setPrefsDialogOpen(false);
-  };
+  const panelMenuOpen = Boolean(panelAnchorEl);
 
   const handleDoubleClick = useCallback(
     (event: React.MouseEvent) => {
@@ -233,10 +222,21 @@ export function AppBar(props: AppBarProps): JSX.Element {
             <IconButton className={cx(classes.logo, classes.noDrag)} size="large" color="inherit">
               <FoxgloveLogo fontSize="inherit" color="inherit" />
             </IconButton>
-            {currentUser != undefined && (
-              <Typography noWrap variant="h5" fontWeight={800} color="inherit">
-                {currentUser.org.displayName}
-              </Typography>
+            {selectedLayoutId != undefined && (
+              <AddPanelIconButton
+                className={classes.iconButton}
+                color="inherit"
+                id="add-panel-button"
+                title="Add panel"
+                aria-label="Add panel button"
+                aria-controls={panelMenuOpen ? "add-panel-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={panelMenuOpen ? "true" : undefined}
+                size="large"
+                onClick={(event) => {
+                  setPanelAnchorEl(event.currentTarget);
+                }}
+              />
             )}
           </div>
 
@@ -271,7 +271,7 @@ export function AppBar(props: AppBarProps): JSX.Element {
                     user: currentUserType,
                     cta: "help-menu",
                   });
-                  handleHelpClick(event);
+                  setPanelAnchorEl(event.currentTarget);
                 }}
               />
               <PreferencesIconButton
@@ -287,7 +287,7 @@ export function AppBar(props: AppBarProps): JSX.Element {
                     user: currentUserType,
                     cta: "preferences-dialog",
                   });
-                  openPreferences();
+                  setPrefsDialogOpen(true);
                 }}
               />
               {!disableSignIn &&
@@ -301,7 +301,7 @@ export function AppBar(props: AppBarProps): JSX.Element {
                     aria-controls={userMenuOpen ? "user-profile-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={userMenuOpen ? "true" : undefined}
-                    onClick={handleUserMenuClick}
+                    onClick={(event) => setPanelAnchorEl(event.currentTarget)}
                     size="small"
                     currentUser={currentUser}
                   />
@@ -335,24 +335,25 @@ export function AppBar(props: AppBarProps): JSX.Element {
           </div>
         </Toolbar>
       </MuiAppBar>
+      <AddPanelMenu
+        anchorEl={panelAnchorEl}
+        open={panelMenuOpen}
+        handleClose={() => setPanelAnchorEl(undefined)}
+      />
       <HelpMenu
         anchorEl={helpAnchorEl}
         open={helpMenuOpen}
-        handleClose={handleHelpClose}
-        anchorOrigin={{
-          horizontal: "right",
-          vertical: "bottom",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
+        handleClose={() => setHelpAnchorEl(undefined)}
       />
-      <UserMenu anchorEl={userAnchorEl} open={userMenuOpen} handleClose={handleUserClose} />
+      <UserMenu
+        anchorEl={userAnchorEl}
+        open={userMenuOpen}
+        handleClose={() => setUserAnchorEl(undefined)}
+      />
       <PreferencesDialog
         id="preferences-dialog"
         open={prefsDialogOpen}
-        onClose={closePreferences}
+        onClose={() => setPrefsDialogOpen(false)}
       />
     </>
   );

packages/studio-base/src/components/PanelList/index.tsx
@@ -11,6 +11,7 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
+import { ReOrderDotsVertical16Filled } from "@fluentui/react-icons";
 import CloseIcon from "@mui/icons-material/Close";
 import SearchIcon from "@mui/icons-material/Search";
 import {
@@ -31,7 +32,7 @@ import {
 } from "@mui/material";
 import fuzzySort from "fuzzysort";
 import { countBy, isEmpty } from "lodash";
-import { useCallback, useEffect, useMemo } from "react";
+import { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useDrag } from "react-dnd";
 import { MosaicDragType, MosaicPath } from "react-mosaic-component";
 import { makeStyles } from "tss-react/mui";
@@ -52,7 +53,7 @@ import {
 } from "@foxglove/studio-base/types/panels";
 import { mightActuallyBePartial } from "@foxglove/studio-base/util/mightActuallyBePartial";
 
-const useStyles = makeStyles()((theme) => {
+const useStyles = makeStyles<void, "dragIcon">()((theme, _params, classes) => {
   return {
     fullHeight: {
       height: "100%",
@@ -64,8 +65,17 @@ const useStyles = makeStyles()((theme) => {
     cardContent: {
       flex: "auto",
     },
-    grab: {
+    listItemButton: {
       cursor: "grab",
+
+      [`&:not(:hover) .${classes.dragIcon}`]: {
+        visibility: "hidden",
+      },
+    },
+    dragIcon: {
+      cursor: "grab",
+      marginRight: theme.spacing(-1),
+      color: theme.palette.text.disabled,
     },
     grid: {
       display: "grid !important",
@@ -77,11 +87,14 @@ const useStyles = makeStyles()((theme) => {
       top: -0.5, // yep that's a half pixel to avoid a gap between the appbar and panel top
       zIndex: 100,
       display: "flex",
-      padding: theme.spacing(2),
+      padding: theme.spacing(1.5),
       justifyContent: "stretch",
       backgroundImage: `linear-gradient(to top, transparent, ${
         theme.palette.background.paper
       } ${theme.spacing(1.5)}) !important`,
+    },
+    toolbarGrid: {
+      padding: theme.spacing(2),
     },
   };
 });
@@ -111,6 +124,7 @@ type PanelItemProps = {
   highlighted?: boolean;
   onClick: () => void;
   mosaicId: string;
+  onDragStart?: () => void;
   onDrop: (arg0: DropDescription) => void;
 };
 
@@ -127,17 +141,21 @@ function DraggablePanelItem({
   searchQuery,
   panel,
   onClick,
+  onDragStart,
   onDrop,
   checked = false,
   highlighted = false,
   mosaicId,
 }: PanelItemProps) {
   const { classes } = useStyles();
-  const scrollRef = React.useRef<HTMLElement>(ReactNull);
+  const scrollRef = useRef<HTMLElement>(ReactNull);
   const [, connectDragSource] = useDrag<unknown, MosaicDropResult, never>({
     type: MosaicDragType.WINDOW,
     // mosaicId is needed for react-mosaic to accept the drop
-    item: () => ({ mosaicId }),
+    item: () => {
+      onDragStart?.();
+      return { mosaicId };
+    },
     options: { dropEffect: "copy" },
     end: (_item, monitor) => {
       const dropResult = monitor.getDropResult() ?? {};
@@ -155,7 +173,7 @@ function DraggablePanelItem({
     },
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (highlighted && scrollRef.current) {
       const highlightedItem = scrollRef.current.getBoundingClientRect();
       const scrollContainer = scrollRef.current.parentElement?.parentElement?.parentElement;
@@ -244,9 +262,10 @@ function DraggablePanelItem({
             </Stack>
           }
         >
-          <ListItem disableGutters disablePadding selected={highlighted}>
+          <ListItem disableGutters disablePadding>
             <ListItemButton
-              className={classes.grab}
+              selected={highlighted}
+              className={classes.listItemButton}
               disabled={checked}
               ref={mergedRef}
               onClick={onClickWithStopPropagation}
@@ -259,6 +278,7 @@ function DraggablePanelItem({
                 }
                 primaryTypographyProps={{ fontWeight: checked ? "bold" : undefined }}
               />
+              <ReOrderDotsVertical16Filled className={classes.dragIcon} />
             </ListItemButton>
           </ListItem>
         </Tooltip>
@@ -277,6 +297,7 @@ export type PanelSelection = {
 type Props = {
   mode?: "grid" | "list";
   onPanelSelect: (arg0: PanelSelection) => void;
+  onDragStart?: () => void;
   selectedPanelType?: string;
 };
 
@@ -303,18 +324,18 @@ function verifyPanels(panels: readonly PanelInfo[]) {
   }
 }
 
-const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) => {
-  const [searchQuery, setSearchQuery] = React.useState("");
-  const [highlightedPanelIdx, setHighlightedPanelIdx] = React.useState<number | undefined>();
-  const { mode, onPanelSelect, selectedPanelType } = props;
-  const { classes } = useStyles();
+const PanelList = forwardRef<HTMLDivElement, Props>((props: Props, ref) => {
+  const { mode, onDragStart, onPanelSelect, selectedPanelType } = props;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [highlightedPanelIdx, setHighlightedPanelIdx] = useState<number | undefined>();
+  const { classes, cx } = useStyles();
 
   const { dropPanel } = useCurrentLayoutActions();
   const mosaicId = usePanelMosaicId();
 
   // Update panel layout when a panel menu item is dropped;
   // actual operations to change layout supplied by react-mosaic-component
-  const onPanelMenuItemDrop = React.useCallback(
+  const onPanelMenuItemDrop = useCallback(
     ({ config, relatedConfigs, type, position, path, tabId }: DropDescription) => {
       dropPanel({
         newPanelType: type,
@@ -329,7 +350,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
     [dropPanel],
   );
 
-  const handleSearchChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearchChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value;
     setSearchQuery(query);
 
@@ -370,7 +391,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
     verifyPanels([...allRegularPanels, ...allPreconfiguredPanels]);
   }, [allRegularPanels, allPreconfiguredPanels]);
 
-  const getFilteredPanels = React.useCallback(
+  const getFilteredPanels = useCallback(
     (panels: PanelInfo[]) => {
       return searchQuery.length > 0
         ? fuzzySort
@@ -386,7 +407,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
     [searchQuery],
   );
 
-  const { filteredRegularPanels, filteredPreconfiguredPanels } = React.useMemo(
+  const { filteredRegularPanels, filteredPreconfiguredPanels } = useMemo(
     () => ({
       filteredRegularPanels: getFilteredPanels(allRegularPanels),
       filteredPreconfiguredPanels: getFilteredPanels(allPreconfiguredPanels),
@@ -394,18 +415,18 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
     [getFilteredPanels, allRegularPanels, allPreconfiguredPanels],
   );
 
-  const allFilteredPanels = React.useMemo(
+  const allFilteredPanels = useMemo(
     () => [...filteredPreconfiguredPanels, ...filteredRegularPanels],
     [filteredPreconfiguredPanels, filteredRegularPanels],
   );
 
-  const highlightedPanel = React.useMemo(() => {
+  const highlightedPanel = useMemo(() => {
     return highlightedPanelIdx != undefined ? allFilteredPanels[highlightedPanelIdx] : undefined;
   }, [allFilteredPanels, highlightedPanelIdx]);
 
   const noResults = allFilteredPanels.length === 0;
 
-  const onKeyDown = React.useCallback(
+  const onKeyDown = useCallback(
     (e: React.KeyboardEvent) => {
       // Prevent key down events from triggering the parent menu, if any.
       if (e.key !== "Escape") {
@@ -445,7 +466,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
     [allFilteredPanels.length, highlightedPanel, mode, onPanelSelect],
   );
 
-  const displayPanelListItem = React.useCallback(
+  const displayPanelListItem = useCallback(
     (panelInfo: PanelInfo) => {
       const { title, type, config, relatedConfigs } = panelInfo;
       return (
@@ -454,6 +475,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
           key={`${type}-${title}`}
           mosaicId={mosaicId}
           panel={panelInfo}
+          onDragStart={onDragStart}
           onDrop={onPanelMenuItemDrop}
           onClick={() => {
             onPanelSelect({ type, config, relatedConfigs });
@@ -469,6 +491,7 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
       highlightedPanel?.title,
       mode,
       mosaicId,
+      onDragStart,
       onPanelMenuItemDrop,
       onPanelSelect,
       searchQuery,
@@ -478,7 +501,11 @@ const PanelList = React.forwardRef<HTMLDivElement, Props>((props: Props, ref) =>
 
   return (
     <div className={classes.fullHeight} ref={ref}>
-      <div className={classes.toolbar}>
+      <div
+        className={cx(classes.toolbar, {
+          [classes.toolbarGrid]: mode === "grid",
+        })}
+      >
         <TextField
           fullWidth
           placeholder="Search panels"

packages/studio-base/src/Workspace.tsx
@@ -526,13 +526,32 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
         "layouts",
         { iconName: "FiveTileGrid", title: "Layouts", component: ConnectedLayoutBrowser },
       ],
-      ["add-panel", { iconName: "RectangularClipping", title: "Add panel", component: AddPanel }],
-      [
-        "panel-settings",
-        { iconName: "PanelSettings", title: "Panel settings", component: PanelSettings },
-      ],
     ]);
 
+    if (!enableNewTopNav) {
+      topItems.set("add-panel", {
+        iconName: "RectangularClipping",
+        title: "Add panel",
+        component: AddPanel,
+      });
+    }
+    topItems.set("panel-settings", {
+      iconName: "PanelSettings",
+      title: "Panel settings",
+      component: PanelSettings,
+    });
+    topItems.set("variables", {
+      iconName: "Variable2",
+      title: "Variables",
+      component: VariablesList,
+    });
+    if (!enableNewTopNav) {
+      topItems.set("extensions", {
+        iconName: "AddIn",
+        title: "Extensions",
+        component: ExtensionsSidebar,
+      });
+    }
     if (enableStudioLogsSidebar) {
       topItems.set("studio-logs-settings", {
         iconName: "BacklogList",
@@ -544,18 +563,6 @@ export default function Workspace(props: WorkspaceProps): JSX.Element {
     const bottomItems = new Map<SidebarItemKey, SidebarItem>([]);
 
     if (!enableNewTopNav) {
-      topItems.set("extensions", {
-        iconName: "Variable2",
-        title: "Variables",
-        component: VariablesList,
-      });
-
-      topItems.set("variables", {
-        iconName: "AddIn",
-        title: "Extensions",
-        component: ExtensionsSidebar,
-      });
-
       if (supportsAccountSettings) {
         bottomItems.set("account", {
           iconName: currentUser != undefined ? "BlockheadFilled" : "Blockhead",

packages/studio-base/src/components/AppBar/AddPanel.tsx
@@ -1,0 +1,57 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { SlideAdd24Regular } from "@fluentui/react-icons";
+import { Menu, IconButton, IconButtonProps, MenuProps } from "@mui/material";
+import { forwardRef } from "react";
+
+import PanelList from "@foxglove/studio-base/components/PanelList";
+import useAddPanel from "@foxglove/studio-base/hooks/useAddPanel";
+
+export const AddPanelIconButton = forwardRef<HTMLButtonElement, IconButtonProps>((props, ref) => {
+  return (
+    <IconButton {...props} ref={ref} id="add-panel-button">
+      <SlideAdd24Regular />
+    </IconButton>
+  );
+});
+
+AddPanelIconButton.displayName = "AddPanelIconButton";
+
+export function AddPanelMenu(
+  props: {
+    handleClose: () => void;
+  } & MenuProps,
+): JSX.Element {
+  const { anchorEl, handleClose, open, ...menuProps } = props;
+  const addPanel = useAddPanel();
+
+  return (
+    <Menu
+      {...menuProps}
+      id="add-panel-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClose}
+      MenuListProps={{
+        disablePadding: true,
+        "aria-labelledby": "add-panel-button",
+      }}
+      anchorOrigin={{
+        horizontal: "left",
+        vertical: "bottom",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+    >
+      <PanelList
+        // Close when a drag starts so the modal menu doesn't block the drop targets
+        onDragStart={handleClose}
+        onPanelSelect={addPanel}
+      />
+    </Menu>
+  );
+}

packages/studio-base/src/components/AppBar/Help.tsx
@@ -2,8 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { ChatHelp24Regular } from "@fluentui/react-icons";
 import CloudOutlinedIcon from "@mui/icons-material/CloudOutlined";
-import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
 import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import {
@@ -41,7 +41,7 @@ const useStyles = makeStyles()((theme) => ({
 export function HelpIconButton(props: IconButtonProps): JSX.Element {
   return (
     <IconButton {...props}>
-      <ContactSupportOutlinedIcon />
+      <ChatHelp24Regular />
     </IconButton>
   );
 }
@@ -51,14 +51,14 @@ export function HelpMenu(
     handleClose: () => void;
   } & MenuProps,
 ): JSX.Element {
-  const { anchorEl, handleClose, open } = props;
+  const { anchorEl, handleClose, open, ...menuProps } = props;
   const { classes } = useStyles();
   const currentUserType = useCurrentUserType();
   const analytics = useAnalytics();
 
   return (
     <Menu
-      {...props}
+      {...menuProps}
       classes={{ paper: classes.paper }}
       id="help-menu"
       anchorEl={anchorEl}
@@ -66,6 +66,14 @@ export function HelpMenu(
       onClose={handleClose}
       MenuListProps={{
         "aria-labelledby": "help-button",
+      }}
+      anchorOrigin={{
+        horizontal: "right",
+        vertical: "bottom",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
       }}
     >
       <ListSubheader className={classes.subheader} tabIndex={-1}>

packages/studio-base/src/components/AppBar/Preferences.tsx
@@ -2,9 +2,9 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { Settings24Regular } from "@fluentui/react-icons";
 import CloseIcon from "@mui/icons-material/Close";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 import {
   Alert,
@@ -175,7 +175,7 @@ export const aboutItems: Map<
 export function PreferencesIconButton(props: IconButtonProps): JSX.Element {
   return (
     <IconButton {...props}>
-      <SettingsOutlinedIcon />
+      <Settings24Regular />
     </IconButton>
   );
 }

packages/studio-base/src/components/AppBar/User.tsx
@@ -34,6 +34,9 @@ const useStyles = makeStyles()((theme) => ({
   avatarButton: {
     padding: 0,
   },
+  menuList: {
+    minWidth: 200,
+  },
   userIconImage: {
     objectFit: "cover",
     width: "100%",
@@ -74,6 +77,7 @@ export function UserMenu({
   anchorEl?: HTMLElement;
   open: boolean;
 }): JSX.Element {
+  const { classes } = useStyles();
   const { currentUser, signOut } = useCurrentUser();
   const { enqueueSnackbar } = useSnackbar();
   const [confirm, confirmModal] = useConfirm();
@@ -113,11 +117,7 @@ export function UserMenu({
         open={open}
         onClose={handleClose}
         onClick={handleClose}
-        MenuListProps={{
-          sx: {
-            minWidth: 200,
-          },
-        }}
+        MenuListProps={{ className: classes.menuList }}
       >
         <MenuItem onClick={onSettingsClick}>
           <ListItemText primary={currentUser.email} />

packages/studio-base/src/components/AppBar/index.stories.tsx
@@ -4,12 +4,20 @@
 
 import { action } from "@storybook/addon-actions";
 import { Story } from "@storybook/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { AppBar } from "@foxglove/studio-base/components/AppBar";
 import MockMessagePipelineProvider from "@foxglove/studio-base/components/MessagePipeline/MockMessagePipelineProvider";
+import Panel from "@foxglove/studio-base/components/Panel";
 import Stack from "@foxglove/studio-base/components/Stack";
 import { User } from "@foxglove/studio-base/context/CurrentUserContext";
+import PanelCatalogContext, {
+  PanelCatalog,
+  PanelInfo,
+} from "@foxglove/studio-base/context/PanelCatalogContext";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
+import MockCurrentLayoutProvider from "@foxglove/studio-base/providers/CurrentLayoutProvider/MockCurrentLayoutProvider";
 
 export default {
   title: "components/AppBar",
@@ -29,11 +37,60 @@ const actions = {
   onCloseWindow: action("onCloseWindow"),
 };
 
+const SamplePanel1 = function () {
+  return <div></div>;
+};
+SamplePanel1.panelType = "sample";
+SamplePanel1.defaultConfig = {};
+
+const SamplePanel2 = function () {
+  return <div></div>;
+};
+SamplePanel2.panelType = "sample2";
+SamplePanel2.defaultConfig = {};
+
+const MockPanel1 = Panel(SamplePanel1);
+const MockPanel2 = Panel(SamplePanel2);
+
+const allPanels: PanelInfo[] = [
+  { title: "Regular Panel BBB", type: "Sample1", module: async () => ({ default: MockPanel1 }) },
+  { title: "Regular Panel AAA", type: "Sample2", module: async () => ({ default: MockPanel2 }) },
+
+  {
+    title: "Preconfigured Panel AAA",
+    type: "Sample1",
+    description: "Panel description",
+    module: async () => ({ default: MockPanel1 }),
+    config: { text: "def" },
+  },
+  {
+    title: "Preconfigured Panel BBB",
+    type: "Sample2",
+    module: async () => ({ default: MockPanel1 }),
+    config: { num: 456 },
+  },
+];
+
+class MockPanelCatalog implements PanelCatalog {
+  public getPanels(): readonly PanelInfo[] {
+    return allPanels;
+  }
+  public getPanelByType(type: string): PanelInfo | undefined {
+    return allPanels.find((panel) => !panel.config && panel.type === type);
+  }
+}
+
 function Wrapper(StoryFn: Story): JSX.Element {
   return (
-    <MockMessagePipelineProvider>
-      <StoryFn />
-    </MockMessagePipelineProvider>
+    <DndProvider backend={HTML5Backend}>
+      <PanelCatalogContext.Provider value={new MockPanelCatalog()}>
+        <MockCurrentLayoutProvider>
+          <MockMessagePipelineProvider>
+            <StoryFn />
+          </MockMessagePipelineProvider>
+        </MockCurrentLayoutProvider>
+      </PanelCatalogContext.Provider>
+    </DndProvider>
   );
 }
 

commit 12f74815335da5c691e7e2219d6e1fdd2b236c9f

Show user menu on click (instead of Add Panel) 🐞 (#5469)
User menu shown instead of Add Panel. bug introduced in #5405

packages/studio-base/src/components/AppBar/index.tsx
@@ -301,7 +301,7 @@ export function AppBar(props: AppBarProps): JSX.Element {
                     aria-controls={userMenuOpen ? "user-profile-menu" : undefined}
                     aria-haspopup="true"
                     aria-expanded={userMenuOpen ? "true" : undefined}
-                    onClick={(event) => setPanelAnchorEl(event.currentTarget)}
+                    onClick={(event) => setUserAnchorEl(event.currentTarget)}
                     size="small"
                     currentUser={currentUser}
                   />