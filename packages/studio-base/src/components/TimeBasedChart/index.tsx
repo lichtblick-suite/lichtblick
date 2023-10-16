lichtblick-suite/lichtblick

commit 05d2afde2ae8a6c001c4812bcb939fc94b5ba19d

Revamp of pipeline for generating plots (#6584)
**User-Facing Changes**
Move all aspects of generating data for plot panels to a Web Worker and
reduce plot generation memory usage.

**Description**
This diff is really scary, but it's divided up into two main sections:
* 70% is related to adding support for `Float32Array`-backed datasets to
our charting components (namely: `Chart` and `TimeBasedChart`). This
greatly reduces the memory overhead of plots with lots of data points.
* 30% is moving all dataset generation and downsampling to a Web Worker

packages/studio-base/src/components/TimeBasedChart/index.tsx
@@ -15,6 +15,7 @@ import { Button, Fade, Tooltip, useTheme } from "@mui/material";
 import { ChartOptions, ScaleOptions } from "chart.js";
 import { AnnotationOptions } from "chartjs-plugin-annotation";
 import { isEqual } from "lodash";
+import * as R from "ramda";
 import React, {
   ComponentProps,
   MouseEvent,
@@ -26,7 +27,6 @@ import React, {
 } from "react";
 import { useMountedState } from "react-use";
 import { makeStyles } from "tss-react/mui";
-import { useDebouncedCallback } from "use-debounce";
 import { v4 as uuidv4 } from "uuid";
 
 import type { ZoomOptions } from "@foxglove/chartjs-plugin-zoom/types/options";
@@ -46,12 +46,14 @@ import {
 import { Bounds } from "@foxglove/studio-base/types/Bounds";
 import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
-import { Downsampler } from "./Downsampler";
 import HoverBar from "./HoverBar";
 import TimeBasedChartTooltipContent, {
   TimeBasedChartTooltipData,
 } from "./TimeBasedChartTooltipContent";
 import { VerticalBarWrapper } from "./VerticalBarWrapper";
+import { ObjectDataProvider, TypedDataProvider } from "./types";
+import useDownsample from "./useDownsampler";
+import useProvider, { getBounds, getTypedBounds, mergeTyped, mergeNormal } from "./useProvider";
 
 const log = Logger.getLogger(__filename);
 
@@ -97,7 +99,10 @@ export type Props = {
   width: number;
   height: number;
   zoom: boolean;
-  data: ChartComponentProps["data"];
+  data?: ChartComponentProps["data"];
+  provider?: ObjectDataProvider;
+  typedData?: ChartComponentProps["typedData"];
+  typedProvider?: TypedDataProvider;
   dataBounds?: Bounds;
   tooltips?: Map<string, TimeBasedChartTooltipData>;
   xAxes?: ScaleOptions<"linear">;
@@ -127,6 +132,9 @@ export default function TimeBasedChart(props: Props): JSX.Element {
   const {
     currentTime,
     data,
+    provider,
+    typedData,
+    typedProvider,
     dataBounds,
     datasetId,
     defaultView,
@@ -140,7 +148,72 @@ export default function TimeBasedChart(props: Props): JSX.Element {
     yAxes,
   } = props;
 
-  const { labels, datasets } = data;
+  const [datasetBounds, setDatasetBounds] = useState<Bounds>({
+    x: {
+      min: 0,
+      max: 0,
+    },
+    y: {
+      min: 0,
+      max: 0,
+    },
+  });
+
+  const [viewportBounds, setViewportBounds] = useState<Bounds>({
+    x: {
+      min: 0,
+      max: 0,
+    },
+    y: {
+      min: 0,
+      max: 0,
+    },
+  });
+
+  const view = React.useMemo(
+    () => ({
+      width,
+      height,
+      bounds: viewportBounds,
+    }),
+    [width, height, viewportBounds],
+  );
+
+  const linesToHide = useMemo(() => props.linesToHide ?? {}, [props.linesToHide]);
+
+  const { downsampler, setScales } = useDownsample(
+    React.useMemo(
+      () =>
+        filterMap(data?.datasets ?? [], (dataset) => {
+          const { label } = dataset;
+          if ((label == undefined || linesToHide[label]) ?? false) {
+            return;
+          }
+          return dataset;
+        }),
+      [data, linesToHide],
+    ),
+  );
+
+  const provided = useProvider(view, getBounds, mergeNormal, data, provider ?? downsampler);
+
+  const typedProvided = useProvider(view, getTypedBounds, mergeTyped, typedData, typedProvider);
+
+  React.useEffect(() => {
+    setDatasetBounds((oldBounds) => {
+      if (provided != undefined && R.equals(oldBounds, provided.bounds)) {
+        return provided.bounds;
+      }
+
+      if (typedProvided != undefined && R.equals(oldBounds, typedProvided.bounds)) {
+        return typedProvided.bounds;
+      }
+
+      return oldBounds;
+    });
+  }, [provided, typedProvided]);
+
+  const bounds = dataBounds ?? datasetBounds;
 
   const theme = useTheme();
   const { classes, cx } = useStyles();
@@ -202,43 +275,8 @@ export default function TimeBasedChart(props: Props): JSX.Element {
     [globalBounds, isSynced],
   );
 
-  const linesToHide = useMemo(() => props.linesToHide ?? {}, [props.linesToHide]);
-
   // some callbacks don't need to re-create when the current scales change, so we keep a ref
   const currentScalesRef = useRef<RpcScales | undefined>(undefined);
-
-  // Calculates the minX/maxX for all our datasets. We do this on the unfiltered datasets
-  // because we need the bounds to properly filter adjacent points. Defers to precomputed
-  // dataBounds, if available.
-  const datasetBounds = useMemo(() => {
-    if (dataBounds) {
-      return dataBounds;
-    }
-
-    let xMin: number | undefined;
-    let xMax: number | undefined;
-    let yMin: number | undefined;
-    let yMax: number | undefined;
-
-    for (const dataset of datasets) {
-      for (const item of dataset.data) {
-        if (item == undefined) {
-          continue;
-        }
-        if (!isNaN(item.x)) {
-          xMin = Math.min(xMin ?? item.x, item.x);
-          xMax = Math.max(xMax ?? item.x, item.x);
-        }
-
-        if (!isNaN(item.x)) {
-          yMin = Math.min(yMin ?? item.y, item.y);
-          yMax = Math.max(yMax ?? item.y, item.y);
-        }
-      }
-    }
-
-    return { x: { min: xMin, max: xMax }, y: { min: yMin, max: yMax } };
-  }, [dataBounds, datasets]);
 
   const onResetZoom = () => {
     setHasUserPannedOrZoomed(false);
@@ -433,8 +471,8 @@ export default function TimeBasedChart(props: Props): JSX.Element {
       max = currentTime ?? 0;
       min = max - defaultView.width;
     } else {
-      min = datasetBounds.x.min;
-      max = datasetBounds.x.max;
+      min = bounds.x.min;
+      max = bounds.x.max;
     }
 
     // If the global bounds are from user interaction, we use that unconditionally.
@@ -453,8 +491,8 @@ export default function TimeBasedChart(props: Props): JSX.Element {
   }, [
     componentId,
     currentTime,
-    datasetBounds.x.max,
-    datasetBounds.x.min,
+    bounds.x.max,
+    bounds.x.min,
     defaultView,
     syncedGlobalBounds,
     hasUserPannedOrZoomed,
@@ -500,10 +538,10 @@ export default function TimeBasedChart(props: Props): JSX.Element {
     // chartjs doesn't like it when only one of min/max are specified for scales
     // so if either is specified then we specify both
     if (maxY == undefined && minY != undefined) {
-      maxY = datasetBounds.y.max;
+      maxY = bounds.y.max;
     }
     if (minY == undefined && maxY != undefined) {
-      minY = datasetBounds.y.min;
+      minY = bounds.y.min;
     }
 
     return {
@@ -516,65 +554,7 @@ export default function TimeBasedChart(props: Props): JSX.Element {
         ...yAxes.ticks,
       },
     } as ScaleOptions;
-  }, [datasetBounds.y, yAxes, theme.palette]);
-
-  // remove datasets that should be hidden
-  const visibleDatasets = useMemo(() => {
-    return filterMap(datasets, (dataset) => {
-      const { label } = dataset;
-      if ((label == undefined || linesToHide[label]) ?? false) {
-        return;
-      }
-      return dataset;
-    });
-  }, [datasets, linesToHide]);
-
-  const [downsampler] = useState(() => new Downsampler());
-
-  // Set the initial downsampler state and downsample the initial dataset
-  const [downsampledDatasets, setDownsampledDatasets] = useState<typeof datasets>(() => {
-    downsampler.update({
-      width,
-      height,
-      datasets: visibleDatasets,
-      datasetBounds,
-      scales: currentScalesRef.current,
-    });
-    return downsampler.downsample();
-  });
-
-  // Stable callback to run the downsampler and update the latest copy of the downsampled datasets
-  const applyDownsample = useCallback(() => {
-    setDownsampledDatasets(downsampler.downsample());
-  }, [downsampler]);
-
-  // Debounce calls to invoke the downsampler
-  const queueDownsample = useDebouncedCallback(
-    applyDownsample,
-    100,
-    // maxWait equal to debounce timeout makes the debounce act like a throttle
-    // Without a maxWait - invocations of the debounced invalidate reset the countdown
-    // resulting in no invalidation when scales are constantly changing (playback)
-    { leading: false, maxWait: 100 },
-  );
-
-  // Updates to the dataset bounds do not need to queue a downsample
-  useEffect(() => {
-    downsampler.update({ datasetBounds });
-  }, [datasetBounds, downsampler]);
-
-  // Updates to the viewport or the datasets queue a downsample
-  useEffect(() => {
-    downsampler.update({ width, height, datasets: visibleDatasets });
-    queueDownsample();
-  }, [downsampler, height, width, visibleDatasets, queueDownsample]);
-
-  const downsampledData = useMemo(() => {
-    return {
-      labels,
-      datasets: downsampledDatasets,
-    };
-  }, [labels, downsampledDatasets]);
+  }, [bounds.y, yAxes, theme.palette]);
 
   const options = useMemo<ChartOptions>(() => {
     return {
@@ -628,10 +608,16 @@ export default function TimeBasedChart(props: Props): JSX.Element {
       }
 
       currentScalesRef.current = scales;
+      if (scales.x != undefined && scales.y != undefined) {
+        const { x, y } = scales;
+        setViewportBounds({
+          x,
+          y,
+        });
+      }
 
       // Scales updated which indicates we might need to adjust the downsampling
-      downsampler.update({ scales });
-      queueDownsample();
+      setScales(scales);
 
       // chart indicated we got a scales update, we may need to update global bounds
       if (!isSynced || !scales.x) {
@@ -694,22 +680,31 @@ export default function TimeBasedChart(props: Props): JSX.Element {
         };
       });
     },
-    [componentId, downsampler, isMounted, isSynced, queueDownsample, setGlobalBounds],
+    [componentId, isMounted, isSynced, setGlobalBounds, setScales],
   );
 
   useEffect(() => log.debug(`<TimeBasedChart> (datasetId=${datasetId})`), [datasetId]);
 
-  const colorsByDatasetIndex = useMemo(() => {
+  const datasets = provided?.data.datasets ?? typedProvided?.data.datasets;
+  const datasetsLength = datasets?.length ?? 0;
+
+  const colorsByDatasetIndex: Record<string, undefined | string> = useMemo(() => {
+    if (datasets == undefined) {
+      return {};
+    }
+
     return Object.fromEntries(
-      data.datasets.map((dataset, index) => [index, dataset.borderColor?.toString()]),
+      datasets.map((dataset, index) => [index, dataset.borderColor?.toString()]),
     );
-  }, [data.datasets]);
+  }, [datasets]);
 
-  const labelsByDatasetIndex = useMemo(() => {
-    return Object.fromEntries(data.datasets.map((dataset, index) => [index, dataset.label]));
-  }, [data.datasets]);
+  const labelsByDatasetIndex: Record<string, undefined | string> = useMemo(() => {
+    if (datasets == undefined) {
+      return {};
+    }
+    return Object.fromEntries(datasets.map((dataset, index) => [index, dataset.label]));
+  }, [datasets]);
 
-  const datasetsLength = datasets.length;
   const tooltipContent = useMemo(() => {
     return activeTooltip ? (
       <TimeBasedChartTooltipContent
@@ -738,7 +733,8 @@ export default function TimeBasedChart(props: Props): JSX.Element {
     height,
     isBoundsReset: globalBounds == undefined,
     options,
-    data: downsampledData,
+    data: provided?.data,
+    typedData: typedProvided?.data,
     onClick: props.onClick,
     onScalesUpdate,
     onStartRender,

packages/studio-base/src/components/Chart/index.tsx
@@ -24,7 +24,7 @@ import Rpc, { createLinkedChannels } from "@foxglove/studio-base/util/Rpc";
 import WebWorkerManager from "@foxglove/studio-base/util/WebWorkerManager";
 import { mightActuallyBePartial } from "@foxglove/studio-base/util/mightActuallyBePartial";
 
-import { ChartData, RpcElement, RpcScales } from "./types";
+import { TypedChartData, ChartData, RpcElement, RpcScales } from "./types";
 
 const log = Logger.getLogger(__filename);
 
@@ -42,7 +42,8 @@ export type OnClickArg = {
 };
 
 type Props = {
-  data: ChartData;
+  data?: ChartData;
+  typedData?: TypedChartData;
   options: ChartOptions;
   isBoundsReset: boolean;
   type: "scatter";
@@ -110,13 +111,23 @@ function Chart(props: Props): JSX.Element {
   const panEnabled =
     (props.options.plugins?.zoom as ZoomPluginOptions | undefined)?.pan?.enabled ?? false;
 
-  const { type, data, isBoundsReset, options, width, height, onStartRender, onFinishRender } =
-    props;
+  const {
+    type,
+    data,
+    typedData,
+    isBoundsReset,
+    options,
+    width,
+    height,
+    onStartRender,
+    onFinishRender,
+  } = props;
 
   const sendWrapperRef = useRef<RpcSend | undefined>();
   const rpcSendRef = useRef<RpcSend | undefined>();
 
   const hasPannedSinceMouseDown = useRef(false);
+  const queuedUpdates = useRef<Partial<ChartUpdateMessage>[]>([]);
   const previousUpdateMessage = useRef<Record<string, unknown>>({});
 
   useLayoutEffect(() => {
@@ -203,6 +214,9 @@ function Chart(props: Props): JSX.Element {
     if (prev.data !== data) {
       prev.data = out.data = data;
     }
+    if (prev.typedData !== typedData) {
+      prev.typedData = out.typedData = typedData;
+    }
     if (prev.options !== options) {
       prev.options = out.options = options;
     }
@@ -221,7 +235,7 @@ function Chart(props: Props): JSX.Element {
     }
 
     return out;
-  }, [data, height, options, isBoundsReset, width]);
+  }, [data, typedData, height, options, isBoundsReset, width]);
 
   // Update the chart with a new set of data
   const updateChart = useCallback(
@@ -253,6 +267,7 @@ function Chart(props: Props): JSX.Element {
             node: offscreenCanvas,
             type,
             data: update.data,
+            typedData: update.typedData,
             options: update.options,
             devicePixelRatio,
             width: update.width,
@@ -265,6 +280,14 @@ function Chart(props: Props): JSX.Element {
           ],
         );
 
+        // Flush any updates that occurred before the worker was initialized
+        const { current: queued } = queuedUpdates;
+        queuedUpdates.current = [];
+        for (const queuedUpdate of queued) {
+          const newScales = await sendWrapperRef.current<RpcScales>("update", queuedUpdate);
+          maybeUpdateScales(newScales);
+        }
+
         // once we are initialized, we can allow other handlers to send to the rpc endpoint
         rpcSendRef.current = sendWrapperRef.current;
 
@@ -273,7 +296,10 @@ function Chart(props: Props): JSX.Element {
         return;
       }
 
-      assert(rpcSendRef.current, "No RPC");
+      if (!rpcSendRef.current) {
+        queuedUpdates.current = [...queuedUpdates.current, update];
+        return;
+      }
 
       onStartRender?.();
       const scales = await rpcSendRef.current<RpcScales>("update", update);
@@ -283,16 +309,9 @@ function Chart(props: Props): JSX.Element {
     [maybeUpdateScales, onFinishRender, onStartRender, type],
   );
 
-  // Prevent updating the chart if we are already updating the chart to avoid backing up stale updates
-  const running = useRef(false);
   const [updateError, setUpdateError] = useState<Error | undefined>();
   useLayoutEffect(() => {
     if (!containerRef.current) {
-      return;
-    }
-
-    // Do we want to queue up this change or do we assume another update will trigger it
-    if (running.current) {
       return;
     }
 
@@ -303,16 +322,11 @@ function Chart(props: Props): JSX.Element {
       return;
     }
 
-    running.current = true;
-    updateChart(newUpdate)
-      .catch((err: Error) => {
-        if (isMounted()) {
-          setUpdateError(err);
-        }
-      })
-      .finally(() => {
-        running.current = false;
-      });
+    updateChart(newUpdate).catch((err: Error) => {
+      if (isMounted()) {
+        setUpdateError(err);
+      }
+    });
   }, [getNewUpdateMessage, isMounted, updateChart]);
 
   useLayoutEffect(() => {

packages/studio-base/src/panels/Plot/index.tsx
@@ -13,10 +13,7 @@
 
 import { isNumber, uniq } from "lodash";
 import { ComponentProps, useCallback, useEffect, useMemo, useState } from "react";
-import { useLatest } from "react-use";
-import { DeepWritable } from "ts-essentials";
 
-import { useShallowMemo } from "@foxglove/hooks";
 import {
   Time,
   add as addTimes,
@@ -24,7 +21,6 @@ import {
   subtract as subtractTimes,
   toSec,
 } from "@foxglove/rostime";
-import { Immutable } from "@foxglove/studio";
 import {
   MessagePipelineContext,
   useMessagePipeline,
@@ -40,8 +36,6 @@ import PanelToolbar, {
 } from "@foxglove/studio-base/components/PanelToolbar";
 import Stack from "@foxglove/studio-base/components/Stack";
 import { ChartDefaultView } from "@foxglove/studio-base/components/TimeBasedChart";
-import { DataSet } from "@foxglove/studio-base/panels/Plot/internalTypes";
-import { usePlotPanelData } from "@foxglove/studio-base/panels/Plot/usePlotPanelData";
 import { OnClickArg as OnChartClickArgs } from "@foxglove/studio-base/src/components/Chart";
 import { OpenSiblingPanel, PanelConfig, SaveConfig } from "@foxglove/studio-base/types/panels";
 import { PANEL_TITLE_CONFIG_KEY } from "@foxglove/studio-base/util/layout";
@@ -49,16 +43,18 @@ import { PANEL_TITLE_CONFIG_KEY } from "@foxglove/studio-base/util/layout";
 import PlotChart from "./PlotChart";
 import { PlotLegend } from "./PlotLegend";
 import { downloadCSV } from "./csv";
+import { TypedDataSet } from "./internalTypes";
+import { EmptyPlotData, EmptyData } from "./plotData";
 import { usePlotPanelSettings } from "./settings";
 import { PlotConfig } from "./types";
+import useDatasets from "./useDatasets";
 
 export { plotableRosTypes } from "./types";
 export type { PlotConfig } from "./types";
 
 const defaultSidebarDimension = 240;
 
-const EmptyDataSets: Immutable<DataSet[]> = Object.freeze([]);
-const EmtpyDataSet: Immutable<DataSet> = Object.freeze({ data: [], label: "" });
+const EmptyDatasets: TypedDataSet[] = [];
 
 export function openSiblingPlotPanel(openSiblingPanel: OpenSiblingPanel, topicName: string): void {
   openSiblingPanel({
@@ -92,10 +88,7 @@ function selectEndTime(ctx: MessagePipelineContext) {
   return ctx.playerState.activeData?.endTime;
 }
 
-// Hack until we can make all the downstream chart types immutable.
-function castWritable<T>(t: T) {
-  return t as DeepWritable<T>;
-}
+const ZERO_TIME = Object.freeze({ sec: 0, nsec: 0 });
 
 function Plot(props: Props) {
   const { saveConfig, config } = props;
@@ -135,8 +128,6 @@ function Plot(props: Props) {
       saveConfig({ paths: [{ value: "", enabled: true, timestampMethod: "receiveTime" }] });
     }
   }, [saveConfig, yAxisPaths.length]);
-
-  const showSingleCurrentMessage = xAxisVal === "currentCustom" || xAxisVal === "index";
 
   const startTime = useMessagePipeline(selectStartTime);
   const currentTime = useMessagePipeline(selectCurrentTime);
@@ -183,17 +174,44 @@ function Plot(props: Props) {
   }, [fixedView, followingView]);
 
   const {
-    bounds: datasetBounds,
-    datasets,
-    pathsWithMismatchedDataLengths,
-  } = usePlotPanelData({
-    followingView,
-    showSingleCurrentMessage,
-    startTime,
-    xAxisVal,
+    data: plotData,
+    provider,
+    getFullData,
+  } = useDatasets({
+    startTime: startTime ?? ZERO_TIME,
+    paths: yAxisPaths,
+    invertedTheme: false,
     xAxisPath,
-    yAxisPaths,
+    xAxisVal,
   });
+
+  const {
+    datasets,
+    bounds: datasetBounds,
+    pathsWithMismatchedDataLengths,
+  } = useMemo(() => {
+    const data = plotData ?? EmptyPlotData;
+    return {
+      bounds: data.bounds,
+      pathsWithMismatchedDataLengths: data.pathsWithMismatchedDataLengths,
+      // Return a dataset for all paths here so that the ordering of datasets corresponds
+      // to yAxisPaths as expected by downstream components like the legend.
+      //
+      // Label is needed so that TimeBasedChart doesn't discard the empty dataset and mess
+      // up the ordering.
+      datasets: yAxisPaths.map((path) => {
+        for (const [otherPath, dataset] of data.datasets.entries()) {
+          if (
+            otherPath.value === path.value &&
+            otherPath.timestampMethod === path.timestampMethod
+          ) {
+            return dataset;
+          }
+        }
+        return { label: path.label ?? path.value, data: [EmptyData] };
+      }),
+    };
+  }, [plotData, yAxisPaths]);
 
   const messagePipeline = useMessagePipelineGetter();
   const onClick = useCallback<NonNullable<ComponentProps<typeof PlotChart>["onClick"]>>(
@@ -222,28 +240,29 @@ function Plot(props: Props) {
     [legendDisplay],
   );
 
-  // Access datasets by latest reference to stabilize our getPanelContextMenuItems callback.
-  const latestDatasets = useLatest(datasets);
-
   const getPanelContextMenuItems = useCallback(() => {
     const items: PanelContextMenuItem[] = [
       {
         type: "item",
         label: "Download plot data as CSV",
-        onclick: () => downloadCSV(latestDatasets.current, xAxisVal),
+        onclick: async () => {
+          // Because the full dataset is never in the rendering thread, we have to request it from the worker.
+          const data = await getFullData();
+          if (data == undefined) {
+            return;
+          }
+          const csvDatasets = [];
+          for (const dataset of data.datasets.values()) {
+            csvDatasets.push(dataset);
+          }
+          downloadCSV(csvDatasets, xAxisVal);
+        },
       },
     ];
     return items;
-  }, [latestDatasets, xAxisVal]);
+  }, [getFullData, xAxisVal]);
 
   const onClickPath = useCallback((index: number) => setFocusedPath(["paths", String(index)]), []);
-
-  // Stabilize datasets we send to the chart by first replacing empty datasets with a constant value
-  // and then shallow memoizing the whole set. This prevents a lot of unnecessary and expensive
-  // rendering while the user is interactively editing paths that don't result in any messages.
-  const stableDatasets = useShallowMemo(
-    datasets.map((ds) => (ds.data.length === 0 ? EmtpyDataSet : ds)),
-  );
 
   return (
     <Stack
@@ -264,7 +283,7 @@ function Plot(props: Props) {
         {legendDisplay !== "none" && (
           <PlotLegend
             currentTime={showPlotValuesInLegend ? currentTimeSinceStart : undefined}
-            datasets={showPlotValuesInLegend ? datasets : EmptyDataSets}
+            datasets={showPlotValuesInLegend ? datasets : EmptyDatasets}
             legendDisplay={legendDisplay}
             onClickPath={onClickPath}
             paths={yAxisPaths}
@@ -279,7 +298,7 @@ function Plot(props: Props) {
           <PlotChart
             currentTime={currentTimeSinceStart}
             datasetBounds={datasetBounds}
-            datasets={castWritable(stableDatasets)}
+            provider={provider}
             defaultView={defaultView}
             isSynced={xAxisVal === "timestamp" && isSynced}
             maxYValue={parseFloat((maxYValue ?? "").toString())}

packages/studio-base/src/PanelAPI/useBlocksSubscriptions.ts
@@ -34,7 +34,10 @@ export type MessageBlock = Immutable<{
 // That said, MessageBlock identity will change when the set of topics changes, so consumers should
 // prefer to use the identity of topic-block message arrays where possible.
 const filterBlockByTopics = memoizeWeak(
-  (block: Immutable<PlayerMessageBlock> | undefined, topics: readonly string[]): MessageBlock => {
+  (
+    block: Immutable<PlayerMessageBlock> | undefined,
+    topics: readonly string[] | SubscribePayload[],
+  ): MessageBlock => {
     if (!block) {
       // For our purposes, a missing MemoryCacheBlock just means "no topics have been cached for
       // this block". This is semantically different to an empty array per topic, but not different
@@ -44,12 +47,13 @@ const filterBlockByTopics = memoizeWeak(
 
     const ret: Record<string, readonly MessageEvent[]> = {};
     for (const topic of topics) {
+      const topicName = typeof topic === "string" ? topic : topic.topic;
       // Don't include an empty array when the data has not been cached for this topic for this
       // block. The missing entry means "we don't know the message for this topic in this block", as
       // opposed to "we know there are no messages for this topic in this block".
-      const blockMessages = block.messagesByTopic[topic];
+      const blockMessages = block.messagesByTopic[topicName];
       if (blockMessages) {
-        ret[topic] = blockMessages;
+        ret[topicName] = blockMessages;
       }
     }
     return ret;

packages/studio-base/src/components/Chart/datasets.test.ts
@@ -1,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { findIndices } from "./datasets";
+
+describe("findIndices", () => {
+  it("ignores empty slices", () => {
+    expect(
+      findIndices(
+        [
+          {
+            x: new Float32Array(),
+            y: new Float32Array(),
+            value: [],
+          },
+          {
+            x: new Float32Array(1),
+            y: new Float32Array(1),
+            value: ["foo"],
+          },
+        ],
+        0,
+      ),
+    ).toEqual([1, 0]);
+  });
+  it("calculates index correctly", () => {
+    expect(
+      findIndices(
+        [
+          {
+            x: new Float32Array(3),
+            y: new Float32Array(3),
+            value: [1, 2, 3],
+          },
+          {
+            x: new Float32Array(1),
+            y: new Float32Array(1),
+            value: [4],
+          },
+        ],
+        3,
+      ),
+    ).toEqual([1, 0]);
+  });
+});

commit 1f1bf0e082e72305f375c788fa8f1da2ecbfc77c

Fix for calculating state transition bounds (#6990)
**User-Facing Changes**
Fix where the state transition panel showed incorrect bounds after
loading data.

**Description**
The code that decided when to update a chart's bounds was incorrect,
which led to some issues.

Regressed in #6584
Fixes #6843

packages/studio-base/src/components/TimeBasedChart/index.tsx
@@ -201,11 +201,11 @@ export default function TimeBasedChart(props: Props): JSX.Element {
 
   React.useEffect(() => {
     setDatasetBounds((oldBounds) => {
-      if (provided != undefined && R.equals(oldBounds, provided.bounds)) {
+      if (provided != undefined && !R.equals(oldBounds, provided.bounds)) {
         return provided.bounds;
       }
 
-      if (typedProvided != undefined && R.equals(oldBounds, typedProvided.bounds)) {
+      if (typedProvided != undefined && !R.equals(oldBounds, typedProvided.bounds)) {
         return typedProvided.bounds;
       }
 