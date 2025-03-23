d6ff914cbaecfa3a236c56b7cb8f661a0319200d
Gabriela Almeida (CTW02164) <gabriela.almeida@ctw.bmwgroup.com>
2025-03-23 11:12:12 +0000

    use loadFiles on uninstall integration test

--- desktop/integration-test/uninstall-extension.test.ts
+++ desktop/integration-test/uninstall-extension.test.ts
@@ -1,9 +1,8 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
 
-import path from "path";
-
 import { launchApp } from "./launchApp";
+import { loadFile } from "./utils/loadFile";
 
 describe("Uninstall extension", () => {
   it("should display 'Uninstalling...' during uninstallation and 'Uninstall' when idle", async () => {
@@ -12,13 +11,10 @@ describe("Uninstall extension", () => {
     /** Install the extension via file upload
      * This step is necessary to ensure the extension is installed before we try to uninstall it
      */
-    const extensionPath = path.resolve(
-      __dirname,
-      "../../packages/suite-base/src/test/fixtures/lichtblick.suite-extension-turtlesim-0.0.1.foxe",
+    await loadFile(
+      app,
+      "../../../packages/suite-base/src/test/fixtures/lichtblick.suite-extension-turtlesim-0.0.1.foxe",
     );
-
-    const fileInput = app.renderer.locator("[data-puppeteer-file-upload]");
-    await fileInput.setInputFiles(extensionPath);
 
     /** End of installation */
 
