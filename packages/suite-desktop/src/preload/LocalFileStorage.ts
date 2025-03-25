507f1f0635c2c2b34fa9ffd1bb35cf0f4c981930
Gabriela Almeida (CTW02164) <gabriela.almeida@ctw.bmwgroup.com>
2025-03-25 15:19:49 +0000

    correct build errors

--- packages/suite-desktop/src/preload/LocalFileStorage.ts
+++ packages/suite-desktop/src/preload/LocalFileStorage.ts
@@ -39,7 +39,7 @@ export default class LocalFileStorage implements Storage {
       for (const entry of await this.list(datastore)) {
         const filePath = path.join(datastoreDir, entry);
         const content = await fs.readFile(filePath);
-        result.push(content);
+        result.push(new Uint8Array(content));
       }
     } catch (err: unknown) {
       if ((err as NodeJS.ErrnoException).code !== "ENOENT") {
@@ -66,12 +66,21 @@ export default class LocalFileStorage implements Storage {
     options?: { encoding?: "utf8" },
   ): Promise<StorageContent | undefined> {
     const filePath = await this.#makeFilePath(datastore, key);
-    return await fs.readFile(filePath, options).catch((err: unknown) => {
-      if ((err as NodeJS.ErrnoException).code !== "ENOENT") {
-        throw err;
-      }
-      return undefined;
-    });
+    return await fs
+      .readFile(filePath, options)
+      .then((content: Buffer | string) => {
+        if (typeof content === "string") {
+          return content;
+        } else {
+          return new Uint8Array(content);
+        }
+      })
+      .catch((err: unknown) => {
+        if ((err as NodeJS.ErrnoException).code !== "ENOENT") {
+          throw err;
+        }
+        return undefined;
+      });
   }
 
   public async put(datastore: string, key: string, value: StorageContent): Promise<void> {
