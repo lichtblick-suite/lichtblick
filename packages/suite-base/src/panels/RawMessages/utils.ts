ddb9e09b659f391bc0f6828a6c1457da0f7484d0
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-03-27 17:39:29 +0000

    code review

--- packages/suite-base/src/panels/RawMessages/utils.ts
+++ packages/suite-base/src/panels/RawMessages/utils.ts
@@ -157,7 +157,9 @@ export function getConstantNameByKeyPath(
   keyPath: (string | number)[],
   queriedData: MessagePathDataItem[],
 ): string | undefined {
-  return keyPath.length > 0 && typeof keyPath[0] === "number"
-    ? queriedData[keyPath[0]]?.constantName
-    : undefined;
+  if (keyPath.length > 0 && typeof keyPath[0] === "number") {
+    return queriedData[keyPath[0]]?.constantName;
+  }
+
+  return undefined;
 }
