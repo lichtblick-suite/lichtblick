dc8640efc88da417c0e5e6f9615a0d5cae8ade9f
Yukihiro Saito <yukky.saito@gmail.com>
2025-04-01 17:31:38 +0900

    cosmetic change :put_litter_in_its_place:
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>

--- packages/suite-base/src/panels/PieChart/types.ts
+++ packages/suite-base/src/panels/PieChart/types.ts
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MPL-2.0
-
 // SPDX-FileCopyrightText: Copyright (C) 2024-2025 Yukihiro Saito <yukky.saito@gmail.com>
 // SPDX-FileCopyrightText: Copyright (C) 2025 Takayuki Honda <takayuki.honda@tier4.jp>
 // SPDX-License-Identifier: MPL-2.0
