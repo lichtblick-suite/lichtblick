aa493ab67bb7aa28c3cefde1bb28b47850af7d49
Luiz Bezerra <luiz.bezerra@ctw.bmwgroup.com>
2025-04-17 15:42:32 +0100

    improved useDiagnostics

--- packages/suite-base/src/panels/diagnostics/useDiagnostics.test.ts
+++ packages/suite-base/src/panels/diagnostics/useDiagnostics.test.ts
@@ -14,51 +14,150 @@
 //   This source code is licensed under the Apache License, Version 2.0,
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
+import { renderHook } from "@testing-library/react";
 
-import { MessageEvent } from "@lichtblick/suite-base/players/types";
+import * as PanelAPI from "@lichtblick/suite-base/PanelAPI";
+import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
+import DiagnosticsBuilder from "@lichtblick/suite-base/testing/builders/DiagnosticsBuilder";
+import MessageEventBuilder from "@lichtblick/suite-base/testing/builders/MessageEventBuilder";
 
-import { addMessages } from "./useDiagnostics";
-import { computeDiagnosticInfo, DiagnosticInfo, DiagnosticStatusArrayMsg, LEVELS } from "./util";
+import useDiagnostics, { addMessages, UseDiagnosticsResult } from "./useDiagnostics";
+import { computeDiagnosticInfo, DiagnosticStatusArrayMsg, LEVELS } from "./util";
 
-const buildMessageAtLevel = (level: number): MessageEvent<DiagnosticStatusArrayMsg> => ({
-  message: {
-    status: [
-      {
-        level,
-        name: "MCTM Logger",
-        message: "No triggers since launch!",
-        hardware_id: "mctm_logger",
-        values: [],
-      },
-    ],
-    header: { stamp: { sec: 1547062466, nsec: 1674890 }, frame_id: "", seq: 0 },
-  },
-  topic: "/foo",
-  receiveTime: { sec: 1547062466, nsec: 1674890 },
-  schemaName: "diagnostic_msgs/Diagnostic",
-  sizeInBytes: 0,
-});
+jest.mock("./util", () => ({
+  ...jest.requireActual("./util"),
+  computeDiagnosticInfo: jest.fn(),
+}));
 
-const diagnosticInfoAtLevel = (level: number): DiagnosticInfo => {
-  const { message } = buildMessageAtLevel(level);
-  return computeDiagnosticInfo(message.status[0]!, message.header.stamp);
-};
+jest.mock("@lichtblick/suite-base/PanelAPI");
 
 describe("addMessages", () => {
-  it("adds a message at the right warning level", () => {
-    const message = buildMessageAtLevel(LEVELS.OK);
-    const info = diagnosticInfoAtLevel(LEVELS.OK);
-    expect(addMessages(new Map(), [message])).toEqual(
-      new Map([[info.status.hardware_id, new Map([[info.status.name, info]])]]),
-    );
+  const computeDiagnosticInfoMock = computeDiagnosticInfo as jest.Mock;
+
+  beforeEach(() => {
+    computeDiagnosticInfoMock.mockReset();
   });
 
-  it("can move a message from one level to another", () => {
-    const message1 = buildMessageAtLevel(LEVELS.OK);
-    const message2 = buildMessageAtLevel(LEVELS.ERROR);
-    const info = diagnosticInfoAtLevel(LEVELS.ERROR);
-    expect(addMessages(new Map(), [message1, message2])).toEqual(
-      new Map([[info.status.hardware_id, new Map([[info.status.name, info]])]]),
-    );
+  it("should add a message at the right level", () => {
+    const level = BasicBuilder.sample(LEVELS);
+    const hardwareId = BasicBuilder.string();
+    const statusMessage = DiagnosticsBuilder.statusMessage({
+      hardware_id: hardwareId,
+      level,
+    });
+    const message = MessageEventBuilder.messageEvent<DiagnosticStatusArrayMsg>({
+      message: DiagnosticsBuilder.statusArrayMsg({
+        status: [statusMessage],
+      }),
+    });
+    const info = DiagnosticsBuilder.info({
+      status: statusMessage,
+    });
+    computeDiagnosticInfoMock.mockReturnValue(info);
+
+    const result = addMessages(new Map(), [message]);
+
+    expect(result).toEqual(new Map([[hardwareId, new Map([[info.status.name, info]])]]));
+  });
+
+  it("should move a message from one level to another", () => {
+    const hardwareId = BasicBuilder.string();
+    // OK LEVEL
+    const okLevel = LEVELS.OK;
+    const statusMessage1 = DiagnosticsBuilder.statusMessage({
+      hardware_id: hardwareId,
+      level: okLevel,
+    });
+    const messageEvent1 = MessageEventBuilder.messageEvent<DiagnosticStatusArrayMsg>({
+      message: DiagnosticsBuilder.statusArrayMsg({
+        status: [statusMessage1],
+      }),
+    });
+    // ERROR LEVEL
+    const errorLevel = LEVELS.ERROR;
+    const statusMessage2 = DiagnosticsBuilder.statusMessage({
+      ...statusMessage1,
+      level: errorLevel,
+    });
+    const messageEvent2 = MessageEventBuilder.messageEvent<DiagnosticStatusArrayMsg>({
+      message: DiagnosticsBuilder.statusArrayMsg({
+        status: [statusMessage2],
+      }),
+    });
+
+    const info1 = DiagnosticsBuilder.info({
+      status: statusMessage1,
+    });
+    const info2 = DiagnosticsBuilder.info({
+      status: statusMessage2,
+    });
+    computeDiagnosticInfoMock.mockReturnValueOnce(info1).mockReturnValueOnce(info2);
+
+    const result = addMessages(new Map(), [messageEvent1, messageEvent2]);
+
+    expect(result).toEqual(new Map([[hardwareId, new Map([[info2.status.name, info2]])]]));
+  });
+
+  it("should return the same map reference if nothing was modified", () => {
+    const hardwareId = BasicBuilder.string();
+    const statusName = BasicBuilder.string();
+    const diagnosticsResult: UseDiagnosticsResult = new Map([
+      [hardwareId, new Map([[statusName, DiagnosticsBuilder.info()]])],
+    ]);
+
+    const result = addMessages(diagnosticsResult, []);
+
+    expect(result).toBe(diagnosticsResult);
+  });
+});
+
+describe("useDiagnostics", () => {
+  const useMessageReducerMock = PanelAPI.useMessageReducer as jest.Mock;
+
+  beforeEach(() => {
+    useMessageReducerMock.mockReset();
+  });
+
+  it("should call useMessageReducer with correct params when topic is provided", () => {
+    const topic = BasicBuilder.string();
+    const mockResult = new Map();
+    useMessageReducerMock.mockReturnValue(mockResult);
+
+    const { result } = renderHook(() => useDiagnostics(topic));
+
+    expect(useMessageReducerMock).toHaveBeenCalledWith({
+      topics: [topic],
+      restore: expect.any(Function),
+      addMessages,
+    });
+    expect(result.current).toBe(mockResult);
+  });
+
+  it("should call useMessageReducer with empty topics when no topic is provided", () => {
+    const mockResult = new Map();
+    useMessageReducerMock.mockReturnValue(mockResult);
+
+    const { result } = renderHook(() => useDiagnostics());
+
+    expect(useMessageReducerMock).toHaveBeenCalledWith({
+      topics: [],
+      restore: expect.any(Function),
+      addMessages,
+    });
+    expect(result.current).toBe(mockResult);
+  });
+
+  it("should return empty map from restore", () => {
+    let restoreFn: () => Map<string, Map<string, unknown>> = () => new Map();
+
+    useMessageReducerMock.mockImplementation(({ restore }) => {
+      restoreFn = restore;
+      return restore();
+    });
+
+    const { result } = renderHook(() => useDiagnostics());
+
+    expect(result.current).toEqual(new Map());
+    expect(restoreFn()).toEqual(new Map());
   });
 });
