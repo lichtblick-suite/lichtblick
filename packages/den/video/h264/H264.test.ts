01fc059b372326c20de0267374184e4727824f50
Alexandre Neuwald <alexandre.neuwald@criticaltechworks.com>
2025-04-23 21:22:53 +0100

    add more tests

--- packages/den/video/h264/H264.test.ts
+++ packages/den/video/h264/H264.test.ts
@@ -86,6 +86,13 @@ describe("H264", () => {
     expect(nalu![0]).toBe(0x67);
   });
 
+  it("GetFirstNALUOfType with boxSize of AnnexB undefined", () => {
+    const INPUT = new Uint8Array([0x00]);
+
+    const nalu = H264.GetFirstNALUOfType(INPUT, 7); // SPS
+    expect(nalu).toBeUndefined();
+  });
+
   it("ParseDecoderConfig", () => {
     const NALU = new Uint8Array([
       0x00, 0x00, 0x01, 0x67, 0x64, 0x0, 0x1e, 0xac, 0xb2, 0x1, 0x40, 0x5f, 0xf2, 0xe0, 0x2d, 0x40,
@@ -97,5 +104,55 @@ describe("H264", () => {
       codedWidth: 640,
       codedHeight: 368,
     });
+  });
+
+  it("ParseDecoderConfig with spsData undefined", () => {
+    const NALU = new Uint8Array([0x00]);
+    const decoderConfig = H264.ParseDecoderConfig(NALU);
+    expect(decoderConfig).toBeUndefined();
+  });
+
+  it("ParseDecoderConfig with nal_unit_type not being SPS", () => {
+    const NALU = new Uint8Array([
+      0x00, 0x00, 0x01, 0x67, 0x64, 0x0, 0x1e, 0xac, 0xb2, 0x1, 0x40, 0x5f, 0xf2, 0xe0, 0x2d, 0x40,
+      0x40, 0x40, 0x50, 0x0, 0x0, 0x3, 0x0, 0x10, 0x0, 0x0, 0x3, 0x3, 0x20, 0xf1, 0x62, 0xe4, 0x80,
+    ]);
+    NALU[0] = 0x67; // Change the nal_unit_type to something other than SPS
+    const decoderConfig = H264.ParseDecoderConfig(NALU);
+    expect(decoderConfig).toBeUndefined();
+  });
+
+  it("IsAnnexB", () => {
+    const annexBData = new Uint8Array([0x00, 0x00, 0x00, 0x01, 0x67]);
+    const nonAnnexBData = new Uint8Array([0x01, 0x67, 0x42, 0x00]);
+
+    expect(H264.IsAnnexB(annexBData)).toBe(true);
+    expect(H264.IsAnnexB(nonAnnexBData)).toBe(false);
+  });
+
+  it("AnnexBBoxSize", () => {
+    const annexB3Bytes = new Uint8Array([0x00, 0x00, 0x01, 0x67]);
+    const annexB4Bytes = new Uint8Array([0x00, 0x00, 0x00, 0x01, 0x67]);
+    const invalidData = new Uint8Array([0x01, 0x67, 0x42]);
+
+    expect(H264.AnnexBBoxSize(annexB3Bytes)).toBe(3);
+    expect(H264.AnnexBBoxSize(annexB4Bytes)).toBe(4);
+    expect(H264.AnnexBBoxSize(invalidData)).toBeUndefined();
+  });
+
+  it("IsKeyframe", () => {
+    const keyframeData = new Uint8Array([
+      0x00, 0x00, 0x00, 0x01, 0x65, 0x88, 0x84, 0x21, 0x00, 0x00, 0x00, 0x01, 0x41,
+    ]); // IDR NALU
+    const nonKeyframeData = new Uint8Array([0x00, 0x00, 0x00, 0x01, 0x61, 0x88, 0x84, 0x21]); // Non-IDR NALU
+
+    expect(H264.IsKeyframe(keyframeData)).toBe(true);
+    expect(H264.IsKeyframe(nonKeyframeData)).toBe(false);
+  });
+
+  it("IsKeyframe, but box size of annexB is undefined", () => {
+    const keyframeData = new Uint8Array([0x00]); // data length < 5
+
+    expect(H264.IsKeyframe(keyframeData)).toBe(false);
   });
 });
