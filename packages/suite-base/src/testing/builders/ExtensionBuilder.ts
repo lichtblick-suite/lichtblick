66effaaa27b25bf97d2b7bc712b8f4528d52c856
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2025-05-07 19:10:25 +0100

    more unit tests

--- packages/suite-base/src/testing/builders/ExtensionBuilder.ts
+++ packages/suite-base/src/testing/builders/ExtensionBuilder.ts
@@ -1,12 +1,13 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
 
+import { ExtensionMarketplaceDetail } from "@lichtblick/suite-base/context/ExtensionMarketplaceContext";
 import BasicBuilder from "@lichtblick/suite-base/testing/builders/BasicBuilder";
 import { defaults } from "@lichtblick/suite-base/testing/builders/utilities";
 import { ExtensionInfo, ExtensionNamespace } from "@lichtblick/suite-base/types/Extensions";
 
 export default class ExtensionBuilder {
-  public static extension(props: Partial<ExtensionInfo> = {}): ExtensionInfo {
+  public static extensionInfo(props: Partial<ExtensionInfo> = {}): ExtensionInfo {
     return defaults<ExtensionInfo>(props, {
       description: BasicBuilder.string(),
       displayName: BasicBuilder.string(),
@@ -21,6 +22,31 @@ export default class ExtensionBuilder {
       version: BasicBuilder.string(),
       readme: BasicBuilder.string(),
       changelog: BasicBuilder.string(),
+    });
+  }
+
+  public static extensionMarketplaceDetail(
+    props: Partial<ExtensionMarketplaceDetail> = {},
+  ): ExtensionMarketplaceDetail {
+    return defaults<ExtensionMarketplaceDetail>(props, {
+      id: BasicBuilder.string(),
+      name: BasicBuilder.string(),
+      qualifiedName: BasicBuilder.string(),
+      description: BasicBuilder.string(),
+      publisher: BasicBuilder.string(),
+      homepage: BasicBuilder.string(),
+      license: BasicBuilder.string(),
+      version: BasicBuilder.string(),
+      readme: BasicBuilder.string(),
+      changelog: BasicBuilder.string(),
+      foxe: BasicBuilder.string(),
+      namespace: BasicBuilder.sample(["local", "org"] as ExtensionNamespace[]),
+      sha256sum: BasicBuilder.string(),
+      keywords: BasicBuilder.strings(),
+      time: {
+        created: BasicBuilder.string(),
+        modified: BasicBuilder.string(),
+      },
     });
   }
 }
