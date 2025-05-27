db99bf9ef6fdf6db6ac8299172dd1a59ff5b7692
Gabriela Almeida (CTW02164) <gabriela.almeida@ctw.bmwgroup.com>
2025-05-27 10:50:39 +0100

    move open map panel after loading a file to new e2e dir

--- e2e/tests/desktop/panel/open-map-panel.desktop.spec.ts
+++ e2e/tests/desktop/panel/open-map-panel.desktop.spec.ts
@@ -1,2 +1,28 @@
-// SPDX-FileCopyrightText: Copyright (C) 2023-2024 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-FileCopyrightText: Copyright (C) 2023-2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
+import { test, expect } from "../../../fixtures/electron";
+import { loadFile } from "../../../fixtures/load-file";
+
+test("open map panel after loading a bag file", async ({ mainWindow }) => {
+  /// Given
+  // A bag file is loaded
+  const filename = "example.bag";
+  await loadFile({
+    mainWindow,
+    filename,
+  });
+
+  // When
+  // Click on Add Panel button
+  await mainWindow.getByTestId("AddPanelButton").click();
+  // And search for "Map" and click on "Map"
+  await mainWindow.getByTestId("panel-menu-item Map").click();
+  // And Click on Panels tab
+  await mainWindow.getByTestId("panel-settings-left").click();
+  // And Click on Map Panel
+  await mainWindow.getByText("Waiting for first GPS point...").nth(0).click();
+
+  // Then
+  // There is a panel on screen named "Map panel"
+  await expect(mainWindow.getByText("Map panel", { exact: true }).count()).resolves.toBe(1);
+});
