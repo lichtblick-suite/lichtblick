41aa9a8cefd8db338957cee521d76c75b86f409f
Tucker925H <takka925h@gmail.com>
2025-05-29 09:44:01 +0900

    feat: implement stateReducer function for PieChart state management

---
+++ packages/suite-base/src/panels/PieChart/utils/stateReducer.ts
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Takayuki Honda <takayuki.honda@tier4.jp>
+// SPDX-License-Identifier: MPL-2.0
+
+import { handleFrame } from "./handleFrame";
+import { handlePath } from "./handlePath";
+import { handleSeek } from "./handleSeek";
+import type { PieChartState, PieChartAction } from "../types";
+
+export function stateReducer(state: PieChartState, action: PieChartAction): PieChartState {
+  switch (action.type) {
+    case "frame":
+      return handleFrame({ state, action });
+    case "path":
+      return handlePath({ state, action });
+    case "seek":
+      return handleSeek(state);
+    default:
+      return state;
+  }
+}
