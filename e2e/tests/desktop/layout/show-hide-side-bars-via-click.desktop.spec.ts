fb0f4a35890e1d2fb4225e1b39855dceec95dd02
Gabriela Almeida (CTW02164) <gabriela.almeida@ctw.bmwgroup.com>
2025-06-06 12:23:35 +0100

    Add e2e tests to show/hide sidebars

---
+++ e2e/tests/desktop/layout/show-hide-side-bars-via-click.desktop.spec.ts
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: Copyright (C) 2023-2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+import { test, expect } from "../../../fixtures/electron";
+
+/**
+ *
+ */
+test("show/hide left side bar via click", async ({ mainWindow }) => {
+  // Given
+  await mainWindow.getByTestId("DataSourceDialog").getByTestId("CloseIcon").click();
+
+  // When
+  await mainWindow.getByTestId("left-sidebar-button").click();
+
+  // Then
+  await expect(mainWindow.getByTestId("panel-settings-left")).not.toBeVisible();
+  await expect(mainWindow.getByTestId("topics-left")).not.toBeVisible();
+  await expect(mainWindow.getByTestId("alerts-left")).not.toBeVisible();
+  await expect(mainWindow.getByTestId("layouts-left")).not.toBeVisible();
+
+  // When
+  await mainWindow.getByTestId("left-sidebar-button").click();
+
+  // Then
+  await expect(mainWindow.getByTestId("panel-settings-left")).toBeVisible();
+  await expect(mainWindow.getByTestId("topics-left")).toBeVisible();
+  await expect(mainWindow.getByTestId("alerts-left")).toBeVisible();
+  await expect(mainWindow.getByTestId("layouts-left")).toBeVisible();
+});
+test("hide/show right side bar via click", async ({ mainWindow }) => {
+  // Given
+  await mainWindow.getByTestId("DataSourceDialog").getByTestId("CloseIcon").click();
+
+  // When
+  await mainWindow.getByTestId("right-sidebar-button").click();
+
+  // Then
+  await expect(mainWindow.getByTestId("variables-right")).toBeVisible();
+
+  // When
+  await mainWindow.getByTestId("right-sidebar-button").click();
+
+  // Then
+  await expect(mainWindow.getByTestId("variables-right")).not.toBeVisible();
+});
