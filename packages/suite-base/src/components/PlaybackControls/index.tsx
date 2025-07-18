ca1093dd499af6f4455a959728be5d929c290c0c
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2025-07-18 15:36:01 +0100

    calling useDirectionalSeek on index, creating index.style.ts file

--- packages/suite-base/src/components/PlaybackControls/index.tsx
+++ packages/suite-base/src/components/PlaybackControls/index.tsx
@@ -29,10 +29,8 @@ import {
 } from "@fluentui/react-icons";
 import { Tooltip } from "@mui/material";
 import { useCallback, useMemo, useState } from "react";
-import { makeStyles } from "tss-react/mui";
 
 import { Time, compare } from "@lichtblick/rostime";
-import { AppSetting } from "@lichtblick/suite-base/AppSetting";
 import { CreateEventDialog } from "@lichtblick/suite-base/components/CreateEventDialog";
 import { DataSourceInfoView } from "@lichtblick/suite-base/components/DataSourceInfoView";
 import EventIcon from "@lichtblick/suite-base/components/EventIcon";
@@ -44,6 +42,8 @@ import {
   useMessagePipeline,
 } from "@lichtblick/suite-base/components/MessagePipeline";
 import SyncInstanceToggle from "@lichtblick/suite-base/components/PlaybackControls/SwitchSyncInstances/SyncInstanceToggle";
+import { useStyles } from "@lichtblick/suite-base/components/PlaybackControls/index.style";
+import { useDirectionalSeek } from "@lichtblick/suite-base/components/PlaybackControls/useDirectionalSeek";
 import PlaybackSpeedControls from "@lichtblick/suite-base/components/PlaybackSpeedControls";
 import Stack from "@lichtblick/suite-base/components/Stack";
 import { useCurrentUser } from "@lichtblick/suite-base/context/BaseUserContext";
@@ -53,44 +53,12 @@ import {
   useWorkspaceStore,
 } from "@lichtblick/suite-base/context/Workspace/WorkspaceContext";
 import { useWorkspaceActions } from "@lichtblick/suite-base/context/Workspace/useWorkspaceActions";
-import { useAppConfigurationValue } from "@lichtblick/suite-base/hooks/useAppConfigurationValue";
 import { Player, PlayerPresence } from "@lichtblick/suite-base/players/types";
 import BroadcastManager from "@lichtblick/suite-base/util/broadcast/BroadcastManager";
 
 import PlaybackTimeDisplay from "./PlaybackTimeDisplay";
 import { RepeatAdapter } from "./RepeatAdapter";
 import Scrubber from "./Scrubber";
-import { DIRECTION, jumpSeek } from "./sharedHelpers";
-
-const useStyles = makeStyles()((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    padding: theme.spacing(0.5, 1, 1, 1),
-    position: "relative",
-    backgroundColor: theme.palette.background.paper,
-    borderTop: `1px solid ${theme.palette.divider}`,
-    zIndex: 100000,
-    overflowX: "auto",
-  },
-  scrubberWrapper: {
-    position: "sticky",
-    top: 0,
-    right: 0,
-    left: 0,
-  },
-  disabled: {
-    opacity: theme.palette.action.disabledOpacity,
-  },
-  popper: {
-    "&[data-popper-placement*=top] .MuiTooltip-tooltip": {
-      margin: theme.spacing(0.5, 0.5, 0.75),
-    },
-  },
-  dataSourceInfoButton: {
-    cursor: "default",
-  },
-}));
 
 const selectPresence = (ctx: MessagePipelineContext) => ctx.playerState.presence;
 const selectEventsSupported = (store: EventsStore) => store.eventsSupported;
@@ -153,61 +121,11 @@ export default function PlaybackControls({
     }
   }, [isPlaying, pause, getTimeInfo, play, seek]);
 
-  const [defaultStepSize] = useAppConfigurationValue<number>(AppSetting.DEFAULT_STEP_SIZE);
-
-  const seekForwardAction = useCallback(
-    (ev?: KeyboardEvent) => {
-      const { currentTime } = getTimeInfo();
-      if (!currentTime) {
-        return;
-      }
-
-      // If playUntil is available, we prefer to use that rather than seek, which performs a jump
-      // seek.
-      //
-      // Playing forward up to the desired seek time will play all messages to the panels which
-      // mirrors the behavior panels would expect when playing without stepping. This behavior is
-      // important for some message types which convey state information.
-      //
-      // i.e. Skipping coordinate frame messages may result in incorrectly rendered markers or
-      // missing markers altogther.
-
-      const targetTime = jumpSeek(DIRECTION.FORWARD, currentTime, ev, defaultStepSize);
-      if (playUntil) {
-        playUntil(targetTime);
-
-        BroadcastManager.getInstance().postMessage({
-          type: "playUntil",
-          time: targetTime,
-        });
-      } else {
-        seek(targetTime);
-
-        BroadcastManager.getInstance().postMessage({
-          type: "seek",
-          time: targetTime,
-        });
-      }
-    },
-    [getTimeInfo, playUntil, seek, defaultStepSize],
-  );
-
-  const seekBackwardAction = useCallback(
-    (ev?: KeyboardEvent) => {
-      const { currentTime } = getTimeInfo();
-      if (!currentTime) {
-        return;
-      }
-      const targetTime = jumpSeek(DIRECTION.BACKWARD, currentTime, ev, defaultStepSize);
-      seek(targetTime);
-
-      BroadcastManager.getInstance().postMessage({
-        type: "seek",
-        time: targetTime,
-      });
-    },
-    [defaultStepSize, getTimeInfo, seek],
-  );
+  const { seekForwardAction, seekBackwardAction } = useDirectionalSeek({
+    seek,
+    getTimeInfo,
+    playUntil,
+  });
 
   const keyDownHandlers = useMemo(
     () => ({
