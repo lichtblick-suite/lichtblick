85c2131e1cb2c039924aae51ea5c75e851afb00f
luiz-bezerra-ctw-bmwgroup-com_QCOM <luiz.bezerra@ctw.bmwgroup.com>
2025-08-05 18:13:59 +0100

    centralized storage keys
    Signed-off-by: luiz-bezerra-ctw-bmwgroup-com_QCOM <luiz.bezerra@ctw.bmwgroup.com>

--- packages/suite-base/src/constants/browserStorageKeys.ts
+++ packages/suite-base/src/constants/browserStorageKeys.ts
@@ -5,4 +5,16 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-export const LOCAL_STORAGE_STUDIO_LAYOUT_KEY = "studio.layout";
+export const KEY_WORKSPACE_PREFIX = process.env.DEV_WORKSPACE
+  ? `${process.env.DEV_WORKSPACE}.`
+  : "";
+
+// Local storage keys
+export const LOCAL_STORAGE_STUDIO_LAYOUT_KEY = `${KEY_WORKSPACE_PREFIX}studio.layout`;
+export const LOCAL_STORAGE_PROFILE_DATA = `${KEY_WORKSPACE_PREFIX}studio.profile-data`;
+export const LOCAL_STORAGE_APP_CONFIGURATION = `${KEY_WORKSPACE_PREFIX}studio.app-configuration.`;
+
+// Session storage keys
+export const SESSION_STORAGE_LOGS_SETTINGS = `${KEY_WORKSPACE_PREFIX}blick.logs-settings`;
+export const SESSION_STORAGE_LICHTBLICK_WORKSPACE = `${KEY_WORKSPACE_PREFIX}fox.workspace`;
+export const SESSION_STORAGE_I18N_LANGUAGE = `${KEY_WORKSPACE_PREFIX}i18nextLng`;
