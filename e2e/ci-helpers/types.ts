47df95ffe3ab8702cb85f8c00b28d89e726eb002
Bezerra Luiz, (Luiz.Bezerra@ctw.bmwgroup.com) <198787532+luiz-bezerra-ctw-bmwgroup-com_QCOM@users.noreply.github.com>
2025-11-13 16:16:57 +0000

    Add SPDX license headers to types.ts file

--- e2e/ci-helpers/types.ts
+++ e2e/ci-helpers/types.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (C) 2023-2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+
 export type ReportTestResult = {
   title: string;
   status: "passed" | "failed" | "skipped" | "timedOut";
