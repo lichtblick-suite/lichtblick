d6e6fabfd26a052de4681c57c108a2030087540c
rodrigo-rodrigues-ctw <rodrigo.PE.rodrigues@ctw.bmwgroup.com>
2025-11-14 16:35:30 +0000

    more changes in develop

--- packages/suite-base/src/panels/Log/LogList.tsx
+++ packages/suite-base/src/panels/Log/LogList.tsx
@@ -107,6 +107,8 @@ function LogList({ items }: LogListProps): React.JSX.Element {
         // eslint-disable-next-line no-restricted-syntax
         console.log('making even more changes 1');
 
+        // eslint-disable-next-line no-restricted-syntax
+        console.log('changes in development1');
         const outerElement = outerRef.current!; // asserted by react-window
 
         // Clear the reset flag - we've processed the scroll event from resetAfterIndex
