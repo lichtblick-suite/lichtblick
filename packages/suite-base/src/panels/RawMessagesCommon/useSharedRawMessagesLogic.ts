3dfbea4b69947b4d032e55988e76be8cb2d7a1ae
Gabriela Almeida <gabriela.almeida@criticaltechworks.com>
2025-12-02 17:23:02 +0000

    correct sonar issues on useSharedRawMessagesLogic

--- packages/suite-base/src/panels/RawMessagesCommon/useSharedRawMessagesLogic.ts
+++ packages/suite-base/src/panels/RawMessagesCommon/useSharedRawMessagesLogic.ts
@@ -133,10 +133,7 @@ export function useSharedRawMessagesLogic<T extends SharedConfig>({
     if (expansion === "all") {
       return false;
     }
-    if (
-      typeof expansion === "object" &&
-      Object.values(expansion).some((v) => v === NodeState.Collapsed)
-    ) {
+    if (typeof expansion === "object" && Object.values(expansion).includes(NodeState.Collapsed)) {
       return true;
     } else {
       return false;
