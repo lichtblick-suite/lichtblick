67253585bfd062eac53cce2276ce5f673397e05c
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2026-01-07 16:55:56 +0000

    testing more tabs funcionalities

--- e2e/tests/desktop/layout/add-tab-to-layout.spec.ts
+++ e2e/tests/desktop/layout/add-tab-to-layout.spec.ts
@@ -5,8 +5,14 @@ import { test, expect } from "../../../fixtures/electron";
 
 /**
  * GIVEN the user is on the layouts tab
- * WHEN they create a new layout and add a panel (e.g., Diagnostics - Details)
- * THEN the new layout should appear with the name "Unnamed layout"
+ * WHEN they create a new layout and add a tab
+ * THEN the new layout should appear with a tab and ready to add more panels
+ * WHEN adding a tab inside the tab parent with a 3d panel
+ * THEN the layout is expected to have two tabs visible (parent and child)
+ * WHEN is added a new tab at child label
+ * THEN there should be three tabs in the layout
+ * WHEN the parent tab is remove
+ * THEN the layout should have zero tabs
  */
 test("create a new layout and add a tab", async ({ mainWindow }) => {
   // Given
@@ -24,4 +30,25 @@ test("create a new layout and add a tab", async ({ mainWindow }) => {
 
   // Then
   await expect(mainWindow.getByTestId("toolbar-tab").count()).resolves.toBe(1);
+  await expect(mainWindow.getByTestId("panel-list-textfield")).toBeVisible();
+
+  // When
+  await panelSearch.fill("tab");
+  await mainWindow.getByRole("button", { name: "Tab Group panels together" }).click();
+  await mainWindow.getByRole("button", { name: "3d" }).click();
+
+  // Then
+  await expect(mainWindow.getByTestId("toolbar-tab").count()).resolves.toBe(2);
+
+  // When
+  await mainWindow.getByTestId("add-tab").nth(1).click();
+
+  // Then
+  await expect(mainWindow.getByTestId("toolbar-tab").count()).resolves.toBe(3);
+
+  // When
+  await mainWindow.getByTestId("CloseIcon").nth(1).click();
+
+  // Then
+  await expect(mainWindow.getByTestId("toolbar-tab").count()).resolves.toBe(0);
 });
