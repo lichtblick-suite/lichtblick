8c9cf5af3f622648421127258d42e20aee8c947b
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2026-01-12 15:46:06 +0000

    organizing test order

--- e2e/tests/desktop/layout/add-tab-to-layout.spec.ts
+++ e2e/tests/desktop/layout/add-tab-to-layout.spec.ts
@@ -7,15 +7,14 @@ import { loadFromFilePicker } from "../../../fixtures/load-from-file-picker";
 const LAYOUT_FILE = "tab-layout.json";
 
 /**
- * GIVEN the user is on the layouts tab
- * WHEN they create a new layout and add a tab
- * THEN the new layout should appear with a tab and ready to add more panels
- * WHEN adding a tab inside the tab parent with a 3d panel
- * THEN the layout is expected to have two tabs visible (parent and child)
- * WHEN is added a new tab at child label
- * THEN there should be three tabs in the layout
- * WHEN the parent tab is remove
- * THEN the layout should have zero tabs
+ * GIVEN the user is on the Layouts tab and a layout containing a tab is loaded
+ * THEN one tab is already visible and ready to accept additional panels
+ * WHEN a tab group is created inside the parent tab using a 3D panel
+ * THEN the layout should display two tabs (parent and child)
+ * WHEN a new tab is added from the child tab label
+ * THEN the layout should display three tabs
+ * WHEN the parent tab is removed
+ * THEN the layout should display zero tabs
  */
 test("create a new layout and add a tab", async ({ mainWindow }) => {
   // Given
@@ -24,27 +23,13 @@ test("create a new layout and add a tab", async ({ mainWindow }) => {
   await loadFromFilePicker(mainWindow, LAYOUT_FILE);
   await mainWindow.getByRole("button", { name: "Import from file…" }).click();
 
-  await expect(
-    mainWindow.getByTestId("layout-list-item").getByText("tab-layout", { exact: true }),
-  ).toBeVisible();
+  //Then
   await expect(mainWindow.getByTestId("toolbar-tab")).toHaveCount(1);
 
   // When
-  // await mainWindow.getByTestId("layout-list-item").getByText("Default", { exact: true }).click();
-  // const createLayoutButton = mainWindow.getByText("Create new layout");
-  // await expect(createLayoutButton).toBeVisible();
-  // await expect(createLayoutButton).toBeEnabled();
-  // await createLayoutButton.click();
-
   const panelSearch = mainWindow.getByTestId("panel-list-textfield").locator("input");
   await panelSearch.fill("tab");
-
   await mainWindow.getByRole("button", { name: "Tab Group panels together" }).click();
-
-  // Then
-  await expect(mainWindow.getByTestId("panel-list-textfield")).toBeVisible();
-
-  // When
   await mainWindow.getByRole("button", { name: "3d" }).click();
 
   // Then
