8e685287dcd36052c42683950eaf0de13917b155
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2026-02-04 17:24:07 +0000

    allow background and modal user

--- packages/suite-base/src/components/PlaybackControls/TimeSlicerModal/TimeSlicerModal.tsx
+++ packages/suite-base/src/components/PlaybackControls/TimeSlicerModal/TimeSlicerModal.tsx
@@ -59,6 +59,13 @@ export default function TimeSlicerModal(props: TimeSlicerModalProps): React.JSX.
       fullWidth
       maxWidth="xs"
       hideBackdrop
+      slotProps={{
+        paper: {
+          sx: {
+            pointerEvents: "auto",
+          },
+        },
+      }}
       className={classes.modal}
     >
       <DialogTitle>Slice Playback Range</DialogTitle>
