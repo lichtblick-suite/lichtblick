fd389912e52f0132aa6b276672169c9cb7c71d86
rodrigo-rodrigues-ctw <rodrigo.pe.rodrigues@ctw.bmwgroup.com>
2026-02-18 15:06:50 +0000

    add unit tests to CameraStateSettings

---
+++ packages/suite-base/src/panels/ThreeDeeRender/renderables/CameraStateSettings.test.ts
@@ -0,0 +1,128 @@
+/** @jest-environment jsdom */
+
+// SPDX-FileCopyrightText: Copyright (C) 2023-2026 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
+// SPDX-License-Identifier: MPL-2.0
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { setupJestCanvasMock } from "jest-canvas-mock";
+
+import { Asset } from "@lichtblick/suite-base/components/PanelExtensionAdapter";
+import { Renderer } from "@lichtblick/suite-base/panels/ThreeDeeRender/Renderer";
+import { DEFAULT_SCENE_EXTENSION_CONFIG } from "@lichtblick/suite-base/panels/ThreeDeeRender/SceneExtensionConfig";
+import { DEFAULT_CAMERA_STATE } from "@lichtblick/suite-base/panels/ThreeDeeRender/camera";
+import { DEFAULT_PUBLISH_SETTINGS } from "@lichtblick/suite-base/panels/ThreeDeeRender/renderables/PublishSettings";
+
+import { RendererConfig } from "../IRenderer";
+import { CameraStateSettings } from "./CameraStateSettings";
+
+jest.mock("three/examples/jsm/libs/draco/draco_decoder.wasm", () => "");
+
+jest.mock("three", () => {
+  const ActualTHREE = jest.requireActual("three");
+  return {
+    ...ActualTHREE,
+    WebGLRenderer: function WebGLRenderer() {
+      return {
+        capabilities: {
+          isWebGL2: true,
+        },
+        setPixelRatio: jest.fn(),
+        setSize: jest.fn(),
+        render: jest.fn(),
+        clear: jest.fn(),
+        setClearColor: jest.fn(),
+        readRenderTargetPixels: jest.fn(),
+        info: {
+          reset: jest.fn(),
+        },
+        shadowMap: {},
+        dispose: jest.fn(),
+        clearDepth: jest.fn(),
+        getDrawingBufferSize: () => ({ width: 100, height: 100 }),
+      };
+    },
+  };
+});
+
+beforeEach(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: undefined,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+const defaultRendererConfig: RendererConfig = {
+  cameraState: DEFAULT_CAMERA_STATE,
+  followMode: "follow-pose",
+  followTf: undefined,
+  scene: {},
+  transforms: {},
+  topics: {},
+  layers: {},
+  publish: DEFAULT_PUBLISH_SETTINGS,
+  imageMode: {},
+};
+
+const fetchAsset = async (uri: string, options?: { signal?: AbortSignal }): Promise<Asset> => {
+  const response = await fetch(uri, options);
+  return {
+    uri,
+    data: new Uint8Array(await response.arrayBuffer()),
+    mediaType: response.headers.get("content-type") ?? undefined,
+  };
+};
+
+const defaultRendererProps = {
+  config: defaultRendererConfig,
+  interfaceMode: "3d" as const,
+  fetchAsset,
+  sceneExtensionConfig: DEFAULT_SCENE_EXTENSION_CONFIG,
+  testOptions: {},
+  customCameraModels: new Map(),
+};
+
+describe("CameraStateSettings", () => {
+  let canvas: HTMLCanvasElement;
+  let parent: HTMLDivElement;
+  let renderer: Renderer;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setupJestCanvasMock();
+    parent = document.createElement("div");
+    canvas = document.createElement("canvas");
+    parent.appendChild(canvas);
+    renderer = new Renderer({ ...defaultRendererProps, canvas });
+  });
+
+  afterEach(() => {
+    renderer.dispose();
+    (console.warn as jest.Mock).mockClear();
+  });
+
+  describe("constructor", () => {
+    it("creates an instance successfully", () => {
+      // Given
+      const aspect = 16 / 9;
+
+      // When
+      const cameraStateSettings = new CameraStateSettings(renderer, canvas, aspect);
+
+      // Then
+      expect(cameraStateSettings).toBeInstanceOf(CameraStateSettings);
+      expect(cameraStateSettings).toBeDefined();
+    });
+  });
+});
