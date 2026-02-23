ce8a9073ef658ea3aaec606b2100cfffb20829d5
Joao Luis <joao.miguel.luis@criticaltechworks.com>
2026-02-23 15:36:14 +0000

    add log

--- packages/suite-base/src/api/extensions/ExtensionsAPI.ts
+++ packages/suite-base/src/api/extensions/ExtensionsAPI.ts
@@ -83,6 +83,7 @@ class ExtensionsAPI implements IExtensionAPI {
   }
 
   public async remove(id: string): Promise<boolean> {
+    console.log(`Removing extension with id ${id} from workspace ${this.workspace}`);
     try {
       await HttpService.delete<IExtensionApiResponse>(
         `${this.workspacePath}/${this.workspace}/extension/${id}`,
