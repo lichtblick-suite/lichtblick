b57506f343d289002a4b83a92e51f0dd2d8319e6
Max Richter <max.richter@understand.ai>
2026-06-19 07:30:30 +0200

    refactor: rename local variable to avoid overlap with parent scope

--- packages/suite-base/src/services/LayoutManager/LayoutManager.ts
+++ packages/suite-base/src/services/LayoutManager/LayoutManager.ts
@@ -460,9 +460,9 @@ export default class LayoutManager implements ILayoutManager {
       };
 
       const result = await this.local.runExclusive(async (local) => {
-        const newLayout = await local.put(localLayoutData);
+        const newLocalLayout = await local.put(localLayoutData);
         await local.put({ ...localLayout, working: undefined });
-        return newLayout;
+        return newLocalLayout;
       });
 
       this.notifyChangeListeners({ type: "change", updatedLayout: result });
