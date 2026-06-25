558f0a55bfe8c0991bfb459e7cd81df1ee4fb0e9
Gabriela Almeida <gabriela.almeida@criticaltechworks.com>
2026-06-25 11:07:26 +0100

    test(raw-messages): fix broken syntax after lint fix

--- packages/suite-base/src/panels/RawMessagesVirtual/flattenTreeData.test.ts
+++ packages/suite-base/src/panels/RawMessagesVirtual/flattenTreeData.test.ts
@@ -117,8 +117,6 @@ describe("flattenTreeData", () => {
       // Then
       expect(result).toHaveLength(3);
       expect(result[0]).toEqual(expect.objectContaining({ key: "0", value: 10 }));
-        expect.objectContaining({ key: "0", value: 10 }),
-      );
     });
 
     it("should return empty array given an empty typed array", () => {
