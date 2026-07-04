d58c996fc7edb0ba3c65a2040d3cd406007ff268
cadkin <adkinscv@ornl.gov>
2026-07-04 17:52:00 -0400

    fix: make glTF meshes load with embedded materials / correct orientation

--- packages/suite-base/src/panels/ThreeDeeRender/renderables/Urdfs.ts
+++ packages/suite-base/src/panels/ThreeDeeRender/renderables/Urdfs.ts
@@ -1095,8 +1095,12 @@ function createRenderable(args: {
     }
     case "mesh": {
       const isCollada = visual.geometry.filename.toLowerCase().endsWith(".dae");
+      const isGLTF = (
+        visual.geometry.filename.toLowerCase().endsWith(".gltf") ||
+        visual.geometry.filename.toLowerCase().endsWith(".glb")
+      )
       // Use embedded materials if the mesh is a Collada file
-      const embedded = isCollada ? EmbeddedMaterialUsage.Use : EmbeddedMaterialUsage.Ignore;
+      const embedded = (isCollada || isGLTF) ? EmbeddedMaterialUsage.Use : EmbeddedMaterialUsage.Ignore;
       const marker = createMeshMarker(frameId, pose, embedded, visual.geometry, baseUrl, color);
       return new RenderableMeshResource(name, marker, undefined, renderer, {
         referenceUrl: baseUrl,
